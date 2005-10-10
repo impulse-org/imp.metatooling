c6bea47e559846d41e8fe9d077a6e822d62f16fa
Philippe Charles <pgc1@rice.edu>
2005-10-10 23:08:39 +0000

    New interface to encapsulate access to the lexer, parser, and current AST.

---
+++ src/org/eclipse/imp/parser/IParseController.java
@@ -0,0 +1,19 @@
+package org.eclipse.uide.parser;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.uide.core.ILanguageService;
+
+public interface IParseController extends ILanguageService
+{
+    IParser getParser();
+    ILexer getLexer();
+    Ast getCurrentAst();
+    boolean isKeyword(int kind);
+    char [][] getKeywords();
+    IASTNodeLocator getNodeLocator();
+    boolean hasErrors();
+    List getErrors();
+    Ast parse (String input, boolean scanOnly, IProgressMonitor monitor);
+}
