dcdac5eaff5e3bf535f1bcabc770aaea55930ff6
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-02-23 02:18:36 +0000

    Now each wizard provides the list of plugin dependencies, rather than one list for everyone.

--- src/org/eclipse/imp/wizards/Wizards.java
+++ src/org/eclipse/imp/wizards/Wizards.java
@@ -1,88 +1,135 @@
 package org.eclipse.uide.wizards;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.uide.runtime.RuntimePlugin;
 
 public class Wizards {
-    public static class GenericServiceWizard extends ExtensionPointWizard {
+    public static abstract class GenericServiceWizard extends ExtensionPointWizard {
 	final static String UIDE_RUNTIME= RuntimePlugin.UIDE_RUNTIME;
 
 	protected void addPages(ExtensionPointWizardPage[] pages) {
 	    super.addPages(pages);
-	    for(int n= 0; n < pages.length; n++) {
-		List requires= pages[n].getRequires();
-		requires.add(UIDE_RUNTIME);
-		// TODO RMF 10/18/2005 -- This dependency info belongs in the extension point wizard page.
-		requires.add("org.eclipse.core.runtime");
-		requires.add("org.eclipse.core.resources");
-		requires.add("org.eclipse.jface.text");
-		requires.add("org.eclipse.ui");
-		requires.add("org.eclipse.ui.ide");
-		requires.add("org.eclipse.ui.editors");
-		requires.add("org.eclipse.ui.views");
-		requires.add("org.eclipse.ui.workbench.texteditor");
-	    }
+            List/*<String>*/ extenRequires= getPluginDependencies();
+            for(Iterator/*<String>*/ iter= extenRequires.iterator(); iter.hasNext(); ) {
+                String requiredPlugin= (String) iter.next();
+                for(int n= 0; n < pages.length; n++) {
+                    List/*<String>*/ pageRequires= pages[n].getRequires();
+                    pageRequires.add(requiredPlugin);
+                }
+            }
+//	    for(int n= 0; n < pages.length; n++) {
+//		List requires= pages[n].getRequires();
+//		requires.add(UIDE_RUNTIME);
+//		// TODO RMF 10/18/2005 -- This dependency info belongs in the extension point wizard page.
+//		requires.add("org.eclipse.core.runtime");
+//		requires.add("org.eclipse.core.resources");
+//		requires.add("org.eclipse.jface.text");
+//		requires.add("org.eclipse.ui");
+//		requires.add("org.eclipse.ui.ide");
+//		requires.add("org.eclipse.ui.editors");
+//		requires.add("org.eclipse.ui.views");
+//		requires.add("org.eclipse.ui.workbench.texteditor");
+//	    }
 	}
+        /**
+         * @return the list of plugin dependencies for this wizard.<br>
+         * Really, this information is specific to the stubs generated for a given language
+         * service extension. So, at least it really belongs to a wizard page, not a wizard.
+         * <br><b>Where to put it???</b>
+         */
+        protected abstract List/*<String>*/ getPluginDependencies();
     }
 
     public static class NewLanguage extends GenericServiceWizard {
 	public void addPages() {
 	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "languageDescription") });
 	}
+        protected List getPluginDependencies() {
+            return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources", "org.eclipse.uide.runtime" });
+        }
     }
 
     public static class NewBuilder extends GenericServiceWizard {
 	public void addPages() {
 	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, "org.eclipse.core.resources", "builders") });
 	}
+        protected List getPluginDependencies() {
+            return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources", "org.eclipse.uide.runtime" });
+        }
     }
 
     public static class NewIndexer extends GenericServiceWizard {
 	public void addPages() {
 	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "index"), });
 	}
+        protected List getPluginDependencies() {
+            return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources", "org.eclipse.uide.runtime" });
+        }
     }
 
     public static class NewParser extends GenericServiceWizard {
 	public void addPages() {
 	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "parser"), });
 	}
+        protected List getPluginDependencies() {
+            return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources", "org.eclipse.uide.runtime" });
+        }
     }
 
     public static class NewModelListener extends GenericServiceWizard {
 	public void addPages() {
 	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "modelListener"), });
 	}
+        protected List getPluginDependencies() {
+            return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources", "org.eclipse.uide.runtime" });
+        }
     }
 
     public static class NewOutliner extends GenericServiceWizard {
 	public void addPages() {
 	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "outliner"), });
 	}
+        protected List getPluginDependencies() {
+            return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources", "org.eclipse.uide.runtime" });
+        }
     }
 
     public static class NewContentProposer extends GenericServiceWizard {
 	public void addPages() {
 	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "contentProposer"), });
 	}
+        protected List getPluginDependencies() {
+            return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources", "org.eclipse.uide.runtime" });
+        }
     }
 
     public static class NewHoverHelper extends GenericServiceWizard {
 	public void addPages() {
 	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "hoverHelper"), });
 	}
+        protected List getPluginDependencies() {
+            return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources", "org.eclipse.uide.runtime" });
+        }
     }
 
     public static class NewTokenColorer extends GenericServiceWizard {
 	public void addPages() {
 	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "tokenColorer"), });
 	}
+        protected List getPluginDependencies() {
+            return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources", "org.eclipse.uide.runtime" });
+        }
     }
 
     public static class NewFoldingUpdater extends GenericServiceWizard {
         public void addPages() {
             addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "foldingUpdater"), });
+        }
+        protected List getPluginDependencies() {
+            return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources", "org.eclipse.uide.runtime" });
         }
     }
 
@@ -90,17 +137,26 @@ public class Wizards {
         public void addPages() {
             addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "autoEditStrategy"), });
         }
+        protected List getPluginDependencies() {
+            return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources", "org.eclipse.uide.runtime" });
+        }
     }
 
     public static class NewHyperlinkDetector extends GenericServiceWizard {
         public void addPages() {
             addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "hyperLink"), });
         }
+        protected List getPluginDependencies() {
+            return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources", "org.eclipse.uide.runtime" });
+        }
     }
 
     public static class NewAnnotationHover extends GenericServiceWizard {
         public void addPages() {
             addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "annotationHover"), });
+        }
+        protected List getPluginDependencies() {
+            return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources", "org.eclipse.uide.runtime" });
         }
     }
 }
