caa61cec4ec457c772d177a3b1a5c790fbb7bc6f
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-02-28 19:50:15 +0000

    Pulled a number of generally useful methods up from one of the derived classes. Now defines an abstract generateCodeStubs() method, called from performFinish(), for derived classes to implement.

--- src/org/eclipse/imp/wizards/ExtensionPointWizard.java
+++ src/org/eclipse/imp/wizards/ExtensionPointWizard.java
@@ -5,25 +5,46 @@ package org.eclipse.uide.wizards;
  * (c) Copyright IBM Corp. 2005  All Rights Reserved
  */
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
 
+import org.eclipse.core.resources.ICommand;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IWorkspaceRunnable;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.uide.WizardPlugin;
 import org.eclipse.uide.core.ErrorHandler;
+import org.eclipse.uide.wizards.Wizards.GenericServiceWizard;
+import org.osgi.framework.Bundle;
 
 /**
  * This wizard creates a new file resource in the provided container. 
  * The wizard creates one file with the extension "g". 
  */
-public class ExtensionPointWizard extends Wizard implements INewWizard {
+public abstract class ExtensionPointWizard extends Wizard implements INewWizard {
     protected int currentPage;
 
     protected ExtensionPointWizardPage pages[];
@@ -63,6 +84,8 @@ public class ExtensionPointWizard extends Wizard implements INewWizard {
 	return super.canFinish();// pages[currentPage].isPageComplete() && (currentPage >= pages.length - 1);
     }
 
+    protected abstract void generateCodeStubs(IProgressMonitor m) throws CoreException;
+
     /**
      * This method is called when 'Finish' button is pressed in the wizard.
      * We will create an operation and run it using wizard as execution context.
@@ -75,8 +98,10 @@ public class ExtensionPointWizard extends Wizard implements INewWizard {
 			try {
 			    for(int n= 0; n < pages.length; n++) {
 				ExtensionPointWizardPage page= pages[n];
+
 				if (!page.hasBeenSkipped())
 				    ExtensionPointEnabler.enable(page, monitor);
+				generateCodeStubs(monitor);
 			    }
 			} catch (Exception e) {
 			    ErrorHandler.reportError("Could not add extension points", e);
@@ -109,4 +134,98 @@ public class ExtensionPointWizard extends Wizard implements INewWizard {
     }
 
     public void init(IWorkbench workbench, IStructuredSelection selection) {}
+
+    protected void addBuilder(IProject project, String id) throws CoreException {
+        IProjectDescription desc= project.getDescription();
+        ICommand[] commands= desc.getBuildSpec();
+        for(int i= 0; i < commands.length; ++i)
+            if (commands[i].getBuilderName().equals(id))
+        	return;
+        //add builder to project
+        ICommand command= desc.newCommand();
+        command.setBuilderName(id);
+        ICommand[] nc= new ICommand[commands.length + 1];
+        // Add it before other builders.
+        System.arraycopy(commands, 0, nc, 1, commands.length);
+        nc[0]= command;
+        desc.setBuildSpec(nc);
+        project.setDescription(desc, null);
+    }
+
+    protected void enableBuilders(IProgressMonitor monitor, final IProject project, final String[] builderIDs) {
+        monitor.setTaskName("Enabling builders...");
+        Job job= new WorkspaceJob("Enabling builders...") {
+            public IStatus runInWorkspace(IProgressMonitor monitor) {
+        	try {
+        	    for(int i= 0; i < builderIDs.length; i++) {
+        		addBuilder(project, builderIDs[i]);
+        	    }
+        	} catch (Throwable e) {
+        	    e.printStackTrace();
+        	}
+        	return Status.OK_STATUS;
+            }
+        };
+        job.schedule();
+    }
+
+    /**
+     * @param monitor
+     * @param file
+     */
+    protected void editFile(IProgressMonitor monitor, final IFile file) {
+        monitor.setTaskName("Opening file for editing...");
+        getShell().getDisplay().asyncExec(new Runnable() {
+            public void run() {
+        	IWorkbenchPage page= PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+        	try {
+        	    IDE.openEditor(page, file, true);
+        	} catch (PartInitException e) {
+        	}
+            }
+        });
+        monitor.worked(1);
+    }
+
+    protected IFile createFileFromTemplate(String fileName, String templateName, String[][] replacements, IProject project, IProgressMonitor monitor) throws CoreException {
+        monitor.beginTask("Creating " + fileName, 2);
+    
+        final IFile file= project.getFile(new Path(fileName));
+        StringBuffer buffer= new StringBuffer(new String(getTemplateFile(templateName)));
+    
+        for(int i= 0; i < replacements.length; i++) {
+            GenericServiceWizard.replace(buffer, replacements[i][0], replacements[i][1]);
+        }
+    
+        if (file.exists()) {
+            file.setContents(new ByteArrayInputStream(buffer.toString().getBytes()), true, true, monitor);
+        } else {
+            file.create(new ByteArrayInputStream(buffer.toString().getBytes()), true, monitor);
+        }
+        monitor.worked(1);
+        return file;
+    }
+
+    protected byte[] getTemplateFile(String fileName) {
+        try {
+            Bundle bundle= Platform.getBundle(getTemplateBundleID());
+            URL url= Platform.asLocalURL(Platform.find(bundle, new Path("/templates/" + fileName)));
+            String path= url.getPath();
+            FileInputStream fis= new FileInputStream(path);
+            DataInputStream is= new DataInputStream(fis);
+            byte bytes[]= new byte[fis.available()];
+    
+            is.readFully(bytes);
+            is.close();
+            fis.close();
+            return bytes;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ("// missing template file: " + fileName).getBytes();
+        }
+    }
+
+    protected String getTemplateBundleID() {
+	return WizardPlugin.kPluginID;
+    }
 }
