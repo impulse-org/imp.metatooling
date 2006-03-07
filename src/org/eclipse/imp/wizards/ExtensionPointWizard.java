064f840f1d2e627cb119a4e971f12345c348aa21
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-03-07 21:48:39 +0000

    Numerous bits of refactoring to hoist common code up from the now-defunct GenericServiceWizard class, to where it can be shared with other wizards that don't derive from GenericServiceWizard (e.g. NewUIDEParserWizard).

--- src/org/eclipse/imp/wizards/ExtensionPointWizard.java
+++ src/org/eclipse/imp/wizards/ExtensionPointWizard.java
@@ -10,10 +10,12 @@ import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IWorkspaceRunnable;
@@ -22,7 +24,6 @@ import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
@@ -39,7 +40,6 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
 import org.eclipse.uide.WizardPlugin;
 import org.eclipse.uide.core.ErrorHandler;
-import org.eclipse.uide.wizards.Wizards.GenericServiceWizard;
 import org.osgi.framework.Bundle;
 
 /**
@@ -68,6 +68,14 @@ public abstract class ExtensionPointWizard extends Wizard implements INewWizard 
 	for(int n= 0; n < pages.length; n++) {
 	    addPage(pages[n]);
 	}
+	List/*<String>*/extenRequires= getPluginDependencies();
+	for(Iterator/*<String>*/iter= extenRequires.iterator(); iter.hasNext();) {
+	    String requiredPlugin= (String) iter.next();
+	    for(int n= 0; n < pages.length; n++) {
+		List/*<String>*/pageRequires= pages[n].getRequires();
+		pageRequires.add(requiredPlugin);
+	    }
+	}
     }
 
     public IWizardPage getPreviousPage(IWizardPage page) {
@@ -85,6 +93,11 @@ public abstract class ExtensionPointWizard extends Wizard implements INewWizard 
     public boolean canFinish() {
 	return super.canFinish();// pages[currentPage].isPageComplete() && (currentPage >= pages.length - 1);
     }
+
+    /**
+     * @return the list of plugin dependencies for this language service.
+     */
+    protected abstract List getPluginDependencies();
 
     /**
      * Generate any necessary code for this extension from template files in the
@@ -108,14 +121,14 @@ public abstract class ExtensionPointWizard extends Wizard implements INewWizard 
      * necessary information from the fields in the various wizard pages needed
      * to generate code.
      */
-    protected void collectCodeParms() { }
+    protected void collectCodeParms() {}
 
     /**
      * This method is called when 'Finish' button is pressed in the wizard.
      * We will create an operation and run it using wizard as execution context.
      */
     public boolean performFinish() {
-        collectCodeParms(); // Do this in the UI thread while the wizard fields are still accessible
+	collectCodeParms(); // Do this in the UI thread while the wizard fields are still accessible
 	IRunnableWithProgress op= new IRunnableWithProgress() {
 	    public void run(IProgressMonitor monitor) throws InvocationTargetException {
 		IWorkspaceRunnable wsop= new IWorkspaceRunnable() {
@@ -161,37 +174,37 @@ public abstract class ExtensionPointWizard extends Wizard implements INewWizard 
     public void init(IWorkbench workbench, IStructuredSelection selection) {}
 
     protected void addBuilder(IProject project, String id) throws CoreException {
-        IProjectDescription desc= project.getDescription();
-        ICommand[] commands= desc.getBuildSpec();
-        for(int i= 0; i < commands.length; ++i)
-            if (commands[i].getBuilderName().equals(id))
-        	return;
-        //add builder to project
-        ICommand command= desc.newCommand();
-        command.setBuilderName(id);
-        ICommand[] nc= new ICommand[commands.length + 1];
-        // Add it before other builders.
-        System.arraycopy(commands, 0, nc, 1, commands.length);
-        nc[0]= command;
-        desc.setBuildSpec(nc);
-        project.setDescription(desc, null);
+	IProjectDescription desc= project.getDescription();
+	ICommand[] commands= desc.getBuildSpec();
+	for(int i= 0; i < commands.length; ++i)
+	    if (commands[i].getBuilderName().equals(id))
+		return;
+	//add builder to project
+	ICommand command= desc.newCommand();
+	command.setBuilderName(id);
+	ICommand[] nc= new ICommand[commands.length + 1];
+	// Add it before other builders.
+	System.arraycopy(commands, 0, nc, 1, commands.length);
+	nc[0]= command;
+	desc.setBuildSpec(nc);
+	project.setDescription(desc, null);
     }
 
     protected void enableBuilders(IProgressMonitor monitor, final IProject project, final String[] builderIDs) {
-        monitor.setTaskName("Enabling builders...");
-        Job job= new WorkspaceJob("Enabling builders...") {
-            public IStatus runInWorkspace(IProgressMonitor monitor) {
-        	try {
-        	    for(int i= 0; i < builderIDs.length; i++) {
-        		addBuilder(project, builderIDs[i]);
-        	    }
-        	} catch (Throwable e) {
-        	    e.printStackTrace();
-        	}
-        	return Status.OK_STATUS;
-            }
-        };
-        job.schedule();
+	monitor.setTaskName("Enabling builders...");
+	Job job= new WorkspaceJob("Enabling builders...") {
+	    public IStatus runInWorkspace(IProgressMonitor monitor) {
+		try {
+		    for(int i= 0; i < builderIDs.length; i++) {
+			addBuilder(project, builderIDs[i]);
+		    }
+		} catch (Throwable e) {
+		    e.printStackTrace();
+		}
+		return Status.OK_STATUS;
+	    }
+	};
+	job.schedule();
     }
 
     /**
@@ -199,72 +212,76 @@ public abstract class ExtensionPointWizard extends Wizard implements INewWizard 
      * @param file
      */
     protected void editFile(IProgressMonitor monitor, final IFile file) {
-        monitor.setTaskName("Opening file for editing...");
-        getShell().getDisplay().asyncExec(new Runnable() {
-            public void run() {
-        	IWorkbenchPage page= PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-        	try {
-        	    IDE.openEditor(page, file, true);
-        	} catch (PartInitException e) {
-        	}
-            }
-        });
-        monitor.worked(1);
+	monitor.setTaskName("Opening file for editing...");
+	getShell().getDisplay().asyncExec(new Runnable() {
+	    public void run() {
+		IWorkbenchPage page= PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		try {
+		    IDE.openEditor(page, file, true);
+		} catch (PartInitException e) {
+		}
+	    }
+	});
+	monitor.worked(1);
     }
 
-    protected IFile createFileFromTemplate(String fileName, String templateName, String folder, String[][] replacements, IProject project, IProgressMonitor monitor) throws CoreException {
-        monitor.beginTask("Creating " + fileName, 2);
-    
-        final IFile file= project.getFile(new Path("src/" + folder + "/" + fileName));
-        StringBuffer buffer= new StringBuffer(new String(getTemplateFile(templateName)));
-    
-        for(int i= 0; i < replacements.length; i++) {
-            GenericServiceWizard.replace(buffer, replacements[i][0], replacements[i][1]);
-        }
-    
-        if (file.exists()) {
-            file.setContents(new ByteArrayInputStream(buffer.toString().getBytes()), true, true, monitor);
-        } else {
-            createSubFolders(folder, project, monitor);
-            file.create(new ByteArrayInputStream(buffer.toString().getBytes()), true, monitor);
-        }
-        monitor.worked(1);
-        return file;
+    protected IFile createFileFromTemplate(String fileName, String templateName, String folder, Map replacements,
+	    IProject project, IProgressMonitor monitor) throws CoreException {
+	monitor.beginTask("Creating " + fileName, 2);
+
+	final IFile file= project.getFile(new Path("src/" + folder + "/" + fileName));
+	String templateContents= new String(getTemplateFile(templateName));
+	String contents= performSubstitutions(templateContents, replacements);
+
+	if (file.exists()) {
+	    file.setContents(new ByteArrayInputStream(contents.getBytes()), true, true, monitor);
+	} else {
+	    file.create(new ByteArrayInputStream(contents.getBytes()), true, monitor);
+	}
+	monitor.worked(1);
+	return file;
     }
 
-    private void createSubFolders(String folder, IProject project, IProgressMonitor monitor) throws CoreException {
-        String[] subFolderNames= folder.split("\\\\");
-        String subFolderStr= "src";
-        for(int i= 0; i < subFolderNames.length; i++) {
-            String childPath= subFolderStr + "/" + subFolderNames[i];
-            Path subFolderPath= new Path(childPath);
-            IFolder subFolder= project.getFolder(subFolderPath);
-            if (!subFolder.exists())
-                subFolder.create(true, true, monitor);
-            subFolderStr= childPath;
-        }
+    public static void replace(StringBuffer sb, String target, String substitute) {
+	for(int index= sb.indexOf(target); index != -1; index= sb.indexOf(target))
+	    sb.replace(index, index + target.length(), substitute);
     }
 
-    protected byte[] getTemplateFile(String fileName) {
-        try {
-            Bundle bundle= Platform.getBundle(getTemplateBundleID());
-            URL url= Platform.asLocalURL(Platform.find(bundle, new Path("/templates/" + fileName)));
-            String path= url.getPath();
-            FileInputStream fis= new FileInputStream(path);
-            DataInputStream is= new DataInputStream(fis);
-            byte bytes[]= new byte[fis.available()];
-    
-            is.readFully(bytes);
-            is.close();
-            fis.close();
-            return bytes;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ("// missing template file: " + fileName).getBytes();
-        }
+    protected String performSubstitutions(String contents, Map replacements) {
+	StringBuffer buffer= new StringBuffer(contents);
+
+	for(Iterator iter= replacements.keySet().iterator(); iter.hasNext();) {
+	    String key= (String) iter.next();
+	    String value= (String) replacements.get(key);
+
+	    if (value != null)
+		replace(buffer, key, value);
+	}
+	return buffer.toString();
     }
 
     protected String getTemplateBundleID() {
 	return WizardPlugin.kPluginID;
     }
+
+    protected byte[] getTemplateFile(String fileName) {
+	try {
+	    Bundle bundle= Platform.getBundle(getTemplateBundleID());
+	    URL url= Platform.asLocalURL(Platform.find(bundle, new Path("/templates/" + fileName)));
+	    String path= url.getPath();
+	    FileInputStream fis= new FileInputStream(path);
+	    DataInputStream is= new DataInputStream(fis);
+	    byte bytes[]= new byte[fis.available()];
+
+	    is.readFully(bytes);
+	    is.close();
+	    fis.close();
+	    return bytes;
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    return ("// missing template file: " + fileName).getBytes();
+	}
+    }
+
+    protected abstract Map getStandardSubstitutions();
 }
--- src/org/eclipse/imp/wizards/Wizards.java
+++ src/org/eclipse/imp/wizards/Wizards.java
@@ -1,92 +1,27 @@
 package org.eclipse.uide.wizards;
 
-import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.net.URL;
 import java.util.Arrays;
-import java.util.Iterator;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.uide.WizardPlugin;
 import org.eclipse.uide.runtime.RuntimePlugin;
 
 public class Wizards {
-    public static abstract class GenericServiceWizard extends ExtensionPointWizard {
-	final static String UIDE_RUNTIME= RuntimePlugin.UIDE_RUNTIME;
-
-	protected void addPages(ExtensionPointWizardPage[] pages) {
-	    super.addPages(pages);
-	    List/*<String>*/extenRequires= getPluginDependencies();
-	    for(Iterator/*<String>*/iter= extenRequires.iterator(); iter.hasNext();) {
-		String requiredPlugin= (String) iter.next();
-		for(int n= 0; n < pages.length; n++) {
-		    List/*<String>*/pageRequires= pages[n].getRequires();
-		    pageRequires.add(requiredPlugin);
-		}
-	    }
-	}
-
-	/**
-	 * @return the list of plugin dependencies for this language service.
-	 */
-	// This information is specific to the stubs generated for a given language
-	// service extension. So, it really belongs to a wizard page, not a wizard.
-	// Where to put it???
-	protected abstract List/*<String>*/getPluginDependencies();
-
-	static public void replace(StringBuffer sb, String target, String substitute) {
-	    for(int index= sb.indexOf(target); index != -1; index= sb.indexOf(target))
-		sb.replace(index, index + target.length(), substitute);
-	}
-
-	protected byte[] getSampleFile(String fileName) {
-	    try {
-		URL url= Platform.asLocalURL(Platform.find(WizardPlugin.getInstance().getBundle(), new Path("/templates/"
-			+ fileName)));
-		String path= url.getPath();
-		FileInputStream fis= new FileInputStream(path);
-		DataInputStream is= new DataInputStream(fis);
-		byte bytes[]= new byte[fis.available()];
-
-		is.readFully(bytes);
-		is.close();
-		fis.close();
-		return bytes;
-	    } catch (Exception e) {
-		e.printStackTrace();
-		return ("// missing template file: " + fileName).getBytes();
-	    }
-	}
-    }
-
-    public static abstract class NoCodeServiceWizard extends GenericServiceWizard {
+    public static abstract class NoCodeServiceWizard extends ExtensionPointWizard {
 	public void generateCodeStubs(IProgressMonitor m) {}
-    }
 
-    // HACK: These must be in sync with the corresponding definitions in NewUIDEParserWizard.
-    static final String astDirectory= "Ast";
-
-    static final String astNode= "ASTNode";
-
-    public static class NewLanguage extends NoCodeServiceWizard {
-	public void addPages() {
-	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "languageDescription") });
-	}
-
-	protected List getPluginDependencies() {
-	    return Arrays.asList(new String[] {
-                    "org.eclipse.core.runtime", "org.eclipse.core.resources",
-		    "org.eclipse.uide.runtime", "org.eclipse.ui" });
+	protected Map getStandardSubstitutions() {
+	    return Collections.EMPTY_MAP; // noone should be calling this: generateCodeStubs() is empty...
 	}
     }
 
-    public static abstract class CodeServiceWizard extends GenericServiceWizard {
+    public static abstract class CodeServiceWizard extends ExtensionPointWizard {
         protected String fLanguageName;
         protected String fPackageName;
         protected String fPackageFolder;
@@ -108,6 +43,31 @@ public class Wizards {
             fParserPackage= buff.toString();
             fClassName= Character.toUpperCase(fLanguageName.charAt(0)) + fLanguageName.substring(1);
         }
+
+        protected Map getStandardSubstitutions() {
+            Map result= new HashMap();
+            result.put("$LANG_NAME$", fLanguageName);
+            result.put("$CLASS_NAME_PREFIX$", fClassName);
+            result.put("$PACKAGE_NAME$", fPackageName);
+            return result;
+        }
+    }
+
+    // HACK: These must be in sync with the corresponding definitions in NewUIDEParserWizard.
+    static final String astDirectory= "Ast";
+
+    static final String astNode= "ASTNode";
+
+    public static class NewLanguage extends NoCodeServiceWizard {
+	public void addPages() {
+	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.UIDE_RUNTIME, "languageDescription") });
+	}
+
+	protected List getPluginDependencies() {
+	    return Arrays.asList(new String[] {
+                    "org.eclipse.core.runtime", "org.eclipse.core.resources",
+		    "org.eclipse.uide.runtime", "org.eclipse.ui" });
+	}
     }
 
     public static class NewBuilder extends CodeServiceWizard {
@@ -124,21 +84,19 @@ public class Wizards {
 	public void generateCodeStubs(IProgressMonitor mon) throws CoreException {
             ExtensionPointWizardPage page= (ExtensionPointWizardPage) pages[0];
             IProject project= page.getProject();
-            String[][] subs= new String[][] {
-                    { "$LANG_NAME$", fLanguageName },
-                    { "$PACKAGE$", fPackageName },
-                    // TODO Should pull the source file-name extension from the language description
-                    { "$EXTEN$", "XXX" }
-            };
+            Map subs= getStandardSubstitutions();
 
-            createFileFromTemplate(pages[0].upperCaseFirst(fLanguageName) + "IncrementalProjectBuilder.java", "builder.tmpl", fPackageFolder, subs, project, mon);
-            createFileFromTemplate(pages[0].upperCaseFirst(fLanguageName) + "Nature.java", "nature.tmpl", fPackageFolder, subs, project, mon);
+            // TODO Should pull the source file-name extension from the language description
+            subs.put("$EXTEN$", "XXX");
+
+            createFileFromTemplate(fClassName + "IncrementalProjectBuilder.java", "builder.tmpl", fPackageFolder, subs, project, mon);
+            createFileFromTemplate(fClassName + "Nature.java", "nature.tmpl", fPackageFolder, subs, project, mon);
 	}
     }
 
     public static class NewIndexer extends NoCodeServiceWizard {
 	public void addPages() {
-	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "index"), });
+	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.UIDE_RUNTIME, "index"), });
 	}
 
 	protected List getPluginDependencies() {
@@ -149,7 +107,7 @@ public class Wizards {
 
     public static class NewParser extends NoCodeServiceWizard {
 	public void addPages() {
-	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "parser"), });
+	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.UIDE_RUNTIME, "parser"), });
 	}
 
 	protected List getPluginDependencies() {
@@ -160,7 +118,7 @@ public class Wizards {
 
     public static class NewModelListener extends NoCodeServiceWizard {
 	public void addPages() {
-	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "modelListener"), });
+	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.UIDE_RUNTIME, "modelListener"), });
 	}
 
 	protected List getPluginDependencies() {
@@ -171,7 +129,7 @@ public class Wizards {
 
     public static class NewOutliner extends CodeServiceWizard {
 	public void addPages() {
-	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "outliner"), });
+	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.UIDE_RUNTIME, "outliner"), });
 	}
 
 	protected List getPluginDependencies() {
@@ -184,21 +142,19 @@ public class Wizards {
 	public void generateCodeStubs(IProgressMonitor mon) throws CoreException {
 	    ExtensionPointWizardPage page= (ExtensionPointWizardPage) pages[0];
 	    IProject project= page.getProject();
-	    String[][] subs= new String[][] {
-	        { "$LANG_NAME$", fLanguageName },
-                { "$PACKAGE$", fPackageName },
-                { "$PARSER_PKG$", fParserPackage },
-                { "$AST_PKG$", fParserPackage + "." + astDirectory },
-                { "$AST_NODE$", astNode }
-	    };
+            Map subs= getStandardSubstitutions();
 
-            createFileFromTemplate(pages[0].upperCaseFirst(fLanguageName) + "Outliner.java", "outliner.tmpl", fPackageFolder, subs, project, mon);
+            subs.put("$PARSER_PKG$", fParserPackage);
+            subs.put("$AST_PKG$", fParserPackage + "." + astDirectory);
+            subs.put("$AST_NODE$", astNode);
+
+            createFileFromTemplate(fClassName + "Outliner.java", "outliner.tmpl", fPackageFolder, subs, project, mon);
 	}
     }
 
-    public static class NewContentProposer extends GenericServiceWizard {
+    public static class NewContentProposer extends CodeServiceWizard {
 	public void addPages() {
-	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "contentProposer"), });
+	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.UIDE_RUNTIME, "contentProposer"), });
 	}
 
 	protected List getPluginDependencies() {
@@ -211,9 +167,9 @@ public class Wizards {
 	}
     }
 
-    public static class NewHoverHelper extends GenericServiceWizard {
+    public static class NewHoverHelper extends CodeServiceWizard {
 	public void addPages() {
-	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "hoverHelper"), });
+	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.UIDE_RUNTIME, "hoverHelper"), });
 	}
 
 	protected List getPluginDependencies() {
@@ -228,7 +184,7 @@ public class Wizards {
 
     public static class NewTokenColorer extends CodeServiceWizard {
 	public void addPages() {
-	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "tokenColorer"), });
+	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.UIDE_RUNTIME, "tokenColorer"), });
 	}
 
 	protected List getPluginDependencies() {
@@ -239,19 +195,18 @@ public class Wizards {
 	public void generateCodeStubs(IProgressMonitor mon) throws CoreException {
             ExtensionPointWizardPage page= (ExtensionPointWizardPage) pages[0];
             IProject project= page.getProject();
-            String[][] subs= new String[][] {
-                { "$LANG_NAME$", fLanguageName },
-                { "$PKG_NAME$", fPackageName },
-                { "$PARSER_PKG$", fParserPackage }
-            };
+            Map subs= getStandardSubstitutions();
 
-            createFileFromTemplate(pages[0].upperCaseFirst(fLanguageName) + "TokenColorer.java", "colorer.tmpl", fPackageFolder, subs, project, mon);
+//          { "$PKG_NAME$", fPackageName },
+            subs.put("$PARSER_PKG$", fParserPackage);
+
+            createFileFromTemplate(fClassName + "TokenColorer.java", "colorer.tmpl", fPackageFolder, subs, project, mon);
 	}
     }
 
-    public static class NewFoldingUpdater extends GenericServiceWizard {
+    public static class NewFoldingUpdater extends CodeServiceWizard {
 	public void addPages() {
-	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "foldingUpdater"), });
+	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.UIDE_RUNTIME, "foldingUpdater"), });
 	}
 
 	protected List getPluginDependencies() {
@@ -264,9 +219,9 @@ public class Wizards {
 	}
     }
 
-    public static class NewAutoEditStrategy extends GenericServiceWizard {
+    public static class NewAutoEditStrategy extends CodeServiceWizard {
 	public void addPages() {
-	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "autoEditStrategy"), });
+	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.UIDE_RUNTIME, "autoEditStrategy"), });
 	}
 
 	protected List getPluginDependencies() {
@@ -279,9 +234,9 @@ public class Wizards {
 	}
     }
 
-    public static class NewHyperlinkDetector extends GenericServiceWizard {
+    public static class NewHyperlinkDetector extends CodeServiceWizard {
 	public void addPages() {
-	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "hyperLink"), });
+	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.UIDE_RUNTIME, "hyperLink"), });
 	}
 
 	protected List getPluginDependencies() {
@@ -294,9 +249,9 @@ public class Wizards {
 	}
     }
 
-    public static class NewAnnotationHover extends GenericServiceWizard {
+    public static class NewAnnotationHover extends CodeServiceWizard {
 	public void addPages() {
-	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, UIDE_RUNTIME, "annotationHover"), });
+	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.UIDE_RUNTIME, "annotationHover"), });
 	}
 
 	protected List getPluginDependencies() {
