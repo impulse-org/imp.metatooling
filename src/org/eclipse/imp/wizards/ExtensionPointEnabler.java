dde40bef9729920e951c0eace281c965c1be15b6
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-03-14 23:37:53 +0000

    Renamed method getPlugin() => getPluginModel(). Added flavor of getPluginModel() that takes a project rather than a wizard page. Added a few public methods to let clients create extensions given the set of extension attributes, rather than driving from a wizard page (probably not yet working fully).

--- src/org/eclipse/imp/wizards/ExtensionPointEnabler.java
+++ src/org/eclipse/imp/wizards/ExtensionPointEnabler.java
@@ -6,10 +6,8 @@ package org.eclipse.uide.wizards;
  */
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.StringBufferInputStream;
 import java.util.List;
-
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
@@ -17,7 +15,6 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.osgi.service.resolver.BundleDescription;
 import org.eclipse.pde.core.IEditableModel;
-import org.eclipse.pde.core.plugin.IExtensions;
 import org.eclipse.pde.core.plugin.IPluginBase;
 import org.eclipse.pde.core.plugin.IPluginExtension;
 import org.eclipse.pde.core.plugin.IPluginImport;
@@ -27,7 +24,6 @@ import org.eclipse.pde.core.plugin.IPluginModelFactory;
 import org.eclipse.pde.core.plugin.ISharedExtensionsModel;
 import org.eclipse.pde.internal.core.PDECore;
 import org.eclipse.pde.internal.core.PluginModelManager;
-import org.eclipse.pde.internal.core.WorkspaceModelManager;
 import org.eclipse.pde.internal.core.ibundle.IBundlePluginModel;
 import org.eclipse.pde.internal.core.plugin.PluginElement;
 import org.eclipse.uide.core.ErrorHandler;
@@ -38,49 +34,73 @@ import org.eclipse.uide.core.ErrorHandler;
 public class ExtensionPointEnabler {
     public static void enable(ExtensionPointWizardPage page, IProgressMonitor monitor) {
 	try {
-	    IPluginModel plugin= getPlugin(page);
-	    if (plugin != null) {
-		addExtensionPoint(plugin, page);
+	    IPluginModel pluginModel= getPluginModel(page);
+
+	    if (pluginModel != null) {
+		addExtensionPoint(pluginModel, page);
 	    }
 	} catch (Exception e) {
 	    ErrorHandler.reportError("Could not enable extension point for " + page, e);
 	}
     }
 
-    private static final String pluginXMLSkeleton= "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-        "<?eclipse version=\"3.0\"?>\n" +
-        "<plugin>\n" +
-        "</plugin>";
+    public static void enable(IProject project, String pluginID, String pointID, String[][] attrNamesValues, IProgressMonitor monitor) {
+	try {
+	    IPluginModel pluginModel= getPluginModelForProject(project);
 
-    public static IPluginModel getPlugin(final ExtensionPointWizardPage page) {
+	    if (pluginModel != null) {
+		addExtensionPoint(pluginModel, pluginID, pointID, attrNamesValues);
+	    }
+	} catch (Exception e) {
+	    ErrorHandler.reportError("Could not enable extension point for " + project.getName(), e);
+	}
+    }
+
+    public static IPluginModel getPluginModel(final ExtensionPointWizardPage page) {
 	try {
 	    final IProject project= page.getProject();
-	    if (project == null) return null;
-            IFile pluginXML= project.getFile("plugin.xml"); 
-            if (!pluginXML.exists())
-                pluginXML.create(new StringBufferInputStream(pluginXMLSkeleton), false, new NullProgressMonitor());
-//            WorkspaceModelManager wmm = PDECore.getDefault().getWorkspaceModelManager();
-//            IPluginModelBase[] wsPlugins= wmm.getFeatureModel(project).getWorkspaceModels();
-            PluginModelManager pmm = PDECore.getDefault().getModelManager();
-            IPluginModelBase[] wsPlugins= pmm.getState().getWorkspaceModels();
 
-            if (wsPlugins.length == 0) {
-                ErrorHandler.reportError("Project " + page.getProject().getName() + " is not a plugin project?", true);
-                return null;
-            }
-            for(int i= 0; i < wsPlugins.length; i++) {
-                IPluginModelBase wsPlugin= wsPlugins[i];
-                if (wsPlugin.getBundleDescription().getName().equals(project.getName())) {
-                    return (IPluginModel) wsPlugin;
-                }
-            }
+	    if (project == null) return null;
+
+            maybeCreatePluginXML(project);
+            return getPluginModelForProject(project);
+	} catch (Exception e) {
+	    ErrorHandler.reportError("Could not find plugin for project " + page.getProject().getName(), true, e);
+	    return null;
+	}
+    }
+
+    private static final String pluginXMLSkeleton= "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+    "<?eclipse version=\"3.0\"?>\n" +
+    "<plugin>\n" +
+    "</plugin>";
+
+    private static void maybeCreatePluginXML(final IProject project) throws CoreException {
+	IFile pluginXML= project.getFile("plugin.xml"); 
+	if (!pluginXML.exists())
+	    pluginXML.create(new StringBufferInputStream(pluginXMLSkeleton), false, new NullProgressMonitor());
+    }
+
+    private static IPluginModel getPluginModelForProject(final IProject project) {
+//      WorkspaceModelManager wmm = PDECore.getDefault().getWorkspaceModelManager();
+//      IPluginModelBase[] wsPlugins= wmm.getFeatureModel(project).getWorkspaceModels();
+	PluginModelManager pmm = PDECore.getDefault().getModelManager();
+	IPluginModelBase[] wsPlugins= pmm.getState().getWorkspaceModels();
+
+	if (wsPlugins.length == 0) {
+	    ErrorHandler.reportError("Project " + project.getName() + " is not a plugin project?", true);
+	    return null;
+	}
+	for(int i= 0; i < wsPlugins.length; i++) {
+	    IPluginModelBase wsPlugin= wsPlugins[i];
+	    if (wsPlugin.getBundleDescription().getName().equals(project.getName())) {
+	        return (IPluginModel) wsPlugin;
+	    }
+	}
 //	    IPluginModelBase thePluginModel= pmm.findModel(project);
 //
 //	    return (IPluginModel) thePluginModel;
-            ErrorHandler.reportError("Could not find plugin for project " + page.getProject().getName(), true);
-	} catch (Exception e) {
-	    ErrorHandler.reportError("Could not find plugin for project " + page.getProject().getName(), true, e);
-	}
+	ErrorHandler.reportError("Could not find plugin for project " + project.getName(), true);
 	return null;
     }
 
@@ -103,7 +123,23 @@ public class ExtensionPointEnabler {
 	saveAndRefresh(pluginModel);
     }
 
-    private static void setElementAttributes(IPluginModel plugin, ExtensionPointWizardPage page, IPluginExtension extension) throws CoreException {
+    public static void addExtensionPoint(IPluginModel pluginModel, String pluginID, String pointID, String[][] attrNamesValues) throws CoreException, IOException {
+	IPluginExtension extension= pluginModel.getPluginFactory().createExtension();
+
+        if (extension == null) {
+            ErrorHandler.reportError("Unable to create extension " + pointID + " in plugin " + pluginModel.getBundleDescription().getName(), true);
+        }
+	extension.setPoint(pluginID + "." + pointID);
+	setElementAttributes(pluginModel, extension, attrNamesValues);
+	// N.B. BundlePluginBase.add(IPluginExtension) has logic to add the "singleton directive" if needed.
+	//      As a result, we call getPluginBase().add() below rather than getExtensions().add()...
+	IPluginBase pluginBase= pluginModel.getPluginBase();
+
+	if (!extension.isInTheModel())
+	    pluginBase.add(extension);
+    }
+
+    private static void setElementAttributes(IPluginModel pluginModel, ExtensionPointWizardPage page, IPluginExtension extension) throws CoreException {
 	PluginElement schema= null;
 	List fields= page.getFields();
 
@@ -112,12 +148,30 @@ public class ExtensionPointEnabler {
 
 	    if (schema == null || !field.schemaName.equals(schema.getName())) {
 		schema= new PluginElement();
-		schema.setModel(plugin);
+		schema.setModel(pluginModel);
 		schema.setName(field.schemaName);
 		extension.add(schema);
 		schema.setParent(extension);
 	    }
 	    schema.setAttribute(field.name, field.value);
+	}
+    }
+
+    public static void setElementAttributes(IPluginModel pluginModel, IPluginExtension extension, String[][] attrNamesValues) throws CoreException {
+	PluginElement elt= null;
+
+	for(int i= 0; i < attrNamesValues.length; i++) {
+	    String attrName= attrNamesValues[i][0];
+	    String attrValue= attrNamesValues[i][1];
+
+	    if (elt == null || !attrName.equals(elt.getName())) {
+		elt= new PluginElement();
+		elt.setModel(pluginModel);
+		elt.setName(attrName);
+		extension.add(elt);
+		elt.setParent(extension);
+	    }
+	    elt.setAttribute(attrName, attrValue);
 	}
     }
 
@@ -155,7 +209,7 @@ public class ExtensionPointEnabler {
 
     static public void addImports(ExtensionPointWizardPage page) {
 	try {
-	    IPluginModel plugin= getPlugin(page);
+	    IPluginModel plugin= getPluginModel(page);
 
 	    if (plugin == null) return;
 
