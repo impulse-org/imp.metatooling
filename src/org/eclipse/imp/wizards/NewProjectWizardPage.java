0c93ba3a959ccfd4f6c80021883bef1e2fafab02
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-03-24 07:48:53 +0000

    Added a "New "New Project Wizard" Wizard" page.

---
+++ src/org/eclipse/imp/wizards/NewProjectWizardPage.java
@@ -0,0 +1,16 @@
+/*
+ * Created on Mar 22, 2006
+ */
+package org.eclipse.uide.wizards;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class NewProjectWizardPage extends ExtensionPointWizardPage {
+    public NewProjectWizardPage(ExtensionPointWizard owner) {
+	super(owner, "org.eclipse.ui", "newWizards");
+    }
+
+    protected void createFirstControls(Composite parent) {
+	createLanguageFieldForPlatformSchema(parent);
+    }
+}
