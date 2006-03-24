057e625db67adbc4991565a80b0b4070574f1ed5
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-03-24 07:50:04 +0000

    Moved code to create "language" field to a separate method, and pulled up to base class for use by other clients.

--- src/org/eclipse/imp/wizards/BuilderWizardPage.java
+++ src/org/eclipse/imp/wizards/BuilderWizardPage.java
@@ -1,9 +1,6 @@
 package org.eclipse.uide.wizards;
 
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Text;
 
 /**
  * Custom wizard page for the extension point "org.eclipse.core.resources.builders",
@@ -15,24 +12,7 @@ public class BuilderWizardPage extends ExtensionPointWizardPage {
         super(owner, "org.eclipse.core.resources", "builders");
     }
 
-    protected void createAdditionalControls(Composite parent) {
-        WizardPageField languageField= new WizardPageField("builders", "language", "Language", "", 0, true, "Language for which to create a builder");
-
-        fLanguageText= createLabelTextBrowse(parent, languageField, null/*"IncrementalProjectBuilder"*/);
-
-        fFields.add(languageField);
-//      // The "value" field for a builder can be empty(?)
-//      getField("value").required= false;
-//      getField("value").label= "Value:";
-        fLanguageText.setData(languageField);
-        fLanguageText.addModifyListener(new ModifyListener() {
-            public void modifyText(ModifyEvent e) {
-                Text text= (Text) e.widget;
-                WizardPageField field= (WizardPageField) text.getData();
-                field.value= text.getText();
-                sLanguage= field.value;
-                dialogChanged();
-            }
-        });
+    protected void createFirstControls(Composite parent) {
+        createLanguageFieldForPlatformSchema(parent);
     }
 }
