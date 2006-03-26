b43672afbd541d76ace6716ee42d2475dcd3aebd
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-03-26 00:26:07 +0000

    Added support for a "poor man's compiler".

--- src/org/eclipse/imp/wizards/Wizards.java
+++ src/org/eclipse/imp/wizards/Wizards.java
@@ -317,4 +317,27 @@ public class Wizards {
 	    // TODO Auto-generated method stub
 	}
     }
+
+    public static class NewCompiler extends CodeServiceWizard {
+        // Need a variant of CodeServiceWizard that doesn't actually create an extension, just generates code...
+        public void addPages() {
+            addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.UIDE_RUNTIME, "compiler"), });
+        }
+
+        protected List getPluginDependencies() {
+            return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources",
+                    "org.eclipse.uide.runtime" });
+        }
+
+        public void generateCodeStubs(IProgressMonitor mon) throws CoreException {
+            ExtensionPointWizardPage page= (ExtensionPointWizardPage) pages[0];
+            IProject project= page.getProject();
+            Map subs= getStandardSubstitutions();
+
+            subs.put("$PARSER_PACKAGE$", fParserPackage); // These should be in the standard substitutions...
+            subs.put("$AST_PACKAGE$", fParserPackage);
+
+            createFileFromTemplate(fClassName + "Compiler.java", "compiler.tmpl", fPackageFolder, subs, project, mon);
+        }
+    }
 }
