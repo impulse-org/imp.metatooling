36c632acb5c5b951fa7b7254f4ffe5a052260770
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-03-28 02:57:30 +0000

    Added a "New Source Formatter" wizard and corresponding template.

--- src/org/eclipse/imp/wizards/Wizards.java
+++ src/org/eclipse/imp/wizards/Wizards.java
@@ -347,4 +347,26 @@ public class Wizards {
             createFileFromTemplate(fClassName + "Compiler.java", "compiler.tmpl", fPackageFolder, subs, project, mon);
         }
     }
+
+    public static class NewFormatter extends CodeServiceWizard {
+        public void addPages() {
+            addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.UIDE_RUNTIME, "formatter"), });
+        }
+
+        protected List getPluginDependencies() {
+            return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources",
+                    "org.eclipse.uide.runtime" });
+        }
+
+        public void generateCodeStubs(IProgressMonitor mon) throws CoreException {
+            ExtensionPointWizardPage page= (ExtensionPointWizardPage) pages[0];
+            IProject project= page.getProject();
+            Map subs= getStandardSubstitutions();
+
+            subs.put("$PARSER_PACKAGE$", fParserPackage); // These should be in the standard substitutions...
+            subs.put("$AST_PACKAGE$", fParserPackage);
+
+            createFileFromTemplate(fClassName + "Formatter.java", "formatter.tmpl", fPackageFolder, subs, project, mon);
+        }
+    }
 }
