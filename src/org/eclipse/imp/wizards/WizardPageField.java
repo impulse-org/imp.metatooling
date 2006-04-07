29739d6ced291cf681c1554e7a82e15dfee9e561
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-04-07 02:30:25 +0000

    Renamed a fName field to fAttributeName for clarity's sake, and added a bunch of JavaDoc comments.

--- src/org/eclipse/imp/wizards/WizardPageField.java
+++ src/org/eclipse/imp/wizards/WizardPageField.java
@@ -8,22 +8,56 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.widgets.Hyperlink;
 
 public class WizardPageField {
-    public String fName;
-    public String fLabel;
-    public int fKind;
-    public boolean fRequired;
-    public String fValue;
-    public String fDescription;
-    public Text fText;
+    /**
+     * Schema element attribute name.
+     */
+    public String fAttributeName;
+
     /**
      * Possibly "qualified schema element" name, indicating the element nesting.
      */
     public String fSchemaElementName;
+
+    /**
+     * The label to be displayed to the left of the attribute's text field.
+     */
+    public String fLabel;
+
+    /**
+     * The attribute's kind according to the extension point schema (i.e., STRING, JAVA, or RESOURCE).
+     */
+    public int fKind;
+
+    /**
+     * If true, this field must have a non-empty value for the dialog's "Finish" button to be enabled.
+     */
+    public boolean fRequired;
+
+    /**
+     * The current value of this attribute.
+     */
+    public String fValue;
+
+    public String fDescription;
+
+    /**
+     * The editable text field holding this attribute's user-specified value
+     */
+    public Text fText;
+
+    /**
+     * The "Browse..." button for this attribute's value (if any; may be null)
+     */
     public Button fButton;
+
+    /**
+     * The hyperlink for the class corresponding to this attribute's value (if any;
+     * may be null if this attribute does not specify a Java class)
+     */
     public Hyperlink fLink;
 
     WizardPageField(String schemaName, String name, String label, String value, int kind, boolean required, String description) {
-        fName= name;
+        fAttributeName= name;
         fLabel= label;
         fValue= value;
         fKind= kind;
@@ -49,6 +83,6 @@ public class WizardPageField {
     }
 
     public String toString() {
-	return "<" + fSchemaElementName + ":" + fName + "=" + fValue + ">";
+	return "<" + fSchemaElementName + ":" + fAttributeName + "=" + fValue + ">";
     }
 }
