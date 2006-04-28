5b20d134f462c477aa2008e4e57cfe2070845a5f
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-04-28 23:23:36 +0000

    Now closer to working with latest changes to base ExtensionPointWizardPage.

--- src/org/eclipse/imp/wizards/NewCompiler.java
+++ src/org/eclipse/imp/wizards/NewCompiler.java
@@ -10,12 +10,29 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.uide.runtime.RuntimePlugin;
 
 public class NewCompiler extends CodeServiceWizard {
     // Need a variant of CodeServiceWizard that doesn't actually create an extension, just generates code...
     public void addPages() {
-        addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.UIDE_RUNTIME, "compiler"), });
+        addPages(new ExtensionPointWizardPage[] { new NewCompilerPage(this) });
+    }
+
+    class NewCompilerPage extends ExtensionPointWizardPage {
+	public NewCompilerPage(ExtensionPointWizard owner) {
+	    super(owner, RuntimePlugin.UIDE_RUNTIME, "compiler", false);
+	}
+
+	protected void createFirstControls(Composite parent) {
+	    createLanguageFieldForPlatformSchema(parent);
+	    WizardPageField field= new WizardPageField(null, "class", "Class:", "Compiler", 0, true, "Name of the compiler implementation class");
+	    Text classText= createLabelTextBrowse(parent, field, "");
+
+	    classText.setData(field);
+	    fFields.add(field);
+	}
     }
 
     protected List getPluginDependencies() {
