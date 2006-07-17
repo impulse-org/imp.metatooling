ea37c2c66c785edf276553f9c003735400546f3d
Stanley M. Sutton <suttons@us.ibm.com>
2006-07-17 18:58:19 +0000

    Makes the wizard actually pay attention to the values that are provided through the wizard.  Also makes use of the new reference-resolver template (which provides a sub-component for the hyperlink detector).

--- src/org/eclipse/imp/wizards/NewHyperlinkDetector.java
+++ src/org/eclipse/imp/wizards/NewHyperlinkDetector.java
@@ -3,8 +3,13 @@
  */
 package org.eclipse.uide.wizards;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.uide.runtime.RuntimePlugin;
@@ -20,6 +25,30 @@ public class NewHyperlinkDetector extends CodeServiceWizard {
     }
 
     public void generateCodeStubs(IProgressMonitor mon) throws CoreException {
-        // TODO Auto-generated method stub
+        ExtensionPointWizardPage page= (ExtensionPointWizardPage) pages[0];
+        IProject project= page.getProject();
+        Map subs= getStandardSubstitutions();
+
+        // SMS 14 Jul 2006:  This bit is from the original
+        // (should check how parameter is used)
+//      { "$PKG_NAME$", fPackageName },
+        subs.put("$PARSER_PKG$", fParserPackage);
+
+        
+        // SMS 14 Jul 2006
+        WizardPageField field = pages[0].getField("class");
+        String qualifiedClassName = field.fValue;
+        String className = qualifiedClassName.substring(qualifiedClassName.lastIndexOf('.')+1) + ".java";
+        subs.remove("$PACKAGE_NAME$");
+        String packageName = qualifiedClassName.substring(0, qualifiedClassName.lastIndexOf('.'));
+        subs.put("$PACKAGE_NAME$", packageName);
+        String packageFolder = packageName.replace('.', File.separatorChar);
+        
+        IFile detectorSrc= createFileFromTemplate(className, "hyperlink_detector.tmpl", packageFolder, subs, project, mon);
+        //IFile resolverSrc = createFileFromTemplate(fClassName + "ReferenceResolver.java", "reference_resolver.tmpl", fPackageFolder, subs, project, mon);
+        IFile resolverSrc = createFileFromTemplate(fClassName + "ReferenceResolver.java", "reference_resolver.tmpl", packageFolder, subs, project, mon);
+        
+        editFile(mon, detectorSrc);
+        editFile(mon, resolverSrc);
     }
 }
