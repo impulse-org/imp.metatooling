11fd60be1b2e3bc58adbb3150e36b9e531ef12dc
Stanley M. Sutton <suttons@us.ibm.com>
2006-08-10 20:25:27 +0000

    Many changes, relating mainly to moving from the framework for new extensions to the (new) framework for new generated components (that aren't extensions--which the compiler isn't).

--- src/org/eclipse/imp/wizards/NewCompiler.java
+++ src/org/eclipse/imp/wizards/NewCompiler.java
@@ -9,51 +9,66 @@ import java.util.List;
 import java.util.Map;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.*;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.uide.runtime.RuntimePlugin;
 
-public class NewCompiler extends CodeServiceWizard {
+import org.eclipse.pde.core.plugin.IExtensions;
+import org.eclipse.pde.core.plugin.IPluginAttribute;
+import org.eclipse.pde.core.plugin.IPluginExtension;
+import org.eclipse.pde.core.plugin.IPluginModelBase;
+import org.eclipse.pde.core.plugin.IPluginObject;
+import org.eclipse.pde.internal.core.ischema.ISchemaAttribute;
+import org.eclipse.pde.internal.core.plugin.PluginElement;
+
+public class NewCompiler extends GeneratedComponentServiceWizard {
     // Need a variant of CodeServiceWizard that doesn't actually create an extension, just generates code...
+	// SMS 27 Jul 2006:  This is it ...
+	
+    protected static String thisWizardName = "New Compiler Wizard";
+    protected static String thisWizardDescription = "Wizard for creating a simple compiler";
+    
+    GeneratedComponentAttribute[] compilerAttributes;
+	
     public void addPages() {
-        addPages(new ExtensionPointWizardPage[] { new NewCompilerPage(this) });
+    	compilerAttributes = setupAttributes();
+        addPages(new GeneratedComponentWizardPage[] { new NewCompilerPage(this) });
     }
 
-    class NewCompilerPage extends ExtensionPointWizardPage {
-	public NewCompilerPage(ExtensionPointWizard owner) {
-	    super(owner, RuntimePlugin.UIDE_RUNTIME, "compiler", false);
-	}
+    
+    class NewCompilerPage extends GeneratedComponentWizardPage {
+		public NewCompilerPage(GeneratedComponentWizard owner) {
+		    super(owner, RuntimePlugin.UIDE_RUNTIME, "compiler", false, compilerAttributes, thisWizardName, thisWizardDescription);
+		}
 
-	protected void createFirstControls(Composite parent) {
-	    createLanguageFieldForPlatformSchema(parent);
-	    WizardPageField field= new WizardPageField(null, "class", "Class:", "Compiler", 0, true, "Name of the compiler implementation class");
-	    Text classText= createLabelTextBrowse(parent, field, "");
-
-	    classText.setData(field);
-	    fFields.add(field);
-	}
+		protected void createFirstControls(Composite parent) {
+		    super.createFirstControls(parent);
+		    createClassField(parent);
+		}
+		
     }
 
+
+    
     protected List getPluginDependencies() {
         return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources",
                 "org.eclipse.uide.runtime" });
     }
 
     public void generateCodeStubs(IProgressMonitor mon) throws CoreException {
-        ExtensionPointWizardPage page= (ExtensionPointWizardPage) pages[0];
+    	GeneratedComponentWizardPage page= (GeneratedComponentWizardPage) pages[0];
         IProject project= page.getProject();
         Map subs= getStandardSubstitutions();
 
-        subs.put("$PARSER_PACKAGE$", fParserPackage); // These should be in the standard substitutions...
-        subs.put("$AST_PACKAGE$", fParserPackage + ".Ast");
-        subs.put("$AST_NODE$", "ASTNode");
-        // SMS 21 Jul 2006
-        // Regarding the above, the parser package is set in the standard
-        // substitutions (at least insofar as being set by CodeServiceWiazrd.
-        // getStandardSubstitutions()) but the others are not--not sure
-        // whether they'll be set anywhere else so as to be in effect here
+        // SMS 10 Aug 2006
+        // The following are all now part of the standard substitution
+        //subs.put("$PARSER_PACKAGE$", fParserPackage); // These should be in the standard substitutions...
+        //subs.put("$AST_PACKAGE$", fParserPackage + ".Ast");
+        //subs.put("$AST_NODE$", "ASTNode");
 
         // SMS 21 Jul 2006
         // Added (or modified) following to accommodate
@@ -71,6 +86,13 @@ public class NewCompiler extends CodeServiceWizard {
         
         String packageFolder = packageName.replace('.', File.separatorChar);
         
+        // SMS 10 Aug 2006
+        // We also need to know the name of the ParseController class
+        // because the user may have changed that from the default
+        String parseControllerClassName = getParseControllerClassName(project);
+        subs.remove("$ParseControllerClassName$");
+        subs.put("$ParseControllerClassName$", parseControllerClassName);
+        
         // SMS 21 Jul 2006
         // NOTE:  The template also makes reference to "$CLASS_NAME_PREFIX$ParseController();"
         // which may *not* be the name of the parese controller if the user has change that
@@ -84,4 +106,133 @@ public class NewCompiler extends CodeServiceWizard {
 
         editFile(mon, compilerSrc);
     }
+ 
+
+    public GeneratedComponentAttribute[] setupAttributes()
+    {
+    	// Warning:  Returning an array with empty elements may cause problems,
+    	// so be sure to only allocate as many elements as there are actual	attributes
+    	GeneratedComponentAttribute[] attributes = new GeneratedComponentAttribute[0];
+    	
+    	// SMS 10 Aug 2006
+    	// Ignore these--they're for testing the attribute mechanism.  The compiler
+    	// doesn't have any attributes that are specific to it, but it's the only
+    	// generated component we have that isn't an extension, so for testing I've
+    	// given it some made-up attributes just so see how things work
+    	// 
+    	/*
+		GeneratedComponentAttribute languageAttr = new GeneratedComponentAttribute();
+		languageAttr.setName("name");
+		languageAttr.setBasedOn("");
+		languageAttr.setDescription("What is the compiler's name?");
+		languageAttr.setValue(null);
+		languageAttr.setUse(ISchemaAttribute.REQUIRED);
+		attributes[0] = languageAttr;
+		
+		GeneratedComponentAttribute classAttr = new GeneratedComponentAttribute();
+		classAttr.setName("quest");
+		classAttr.setBasedOn("");
+		classAttr.setDescription("What is the compiler's quest?");
+		classAttr.setValue(null);
+		classAttr.setUse(ISchemaAttribute.REQUIRED);
+		attributes[1] = classAttr;
+		
+		GeneratedComponentAttribute colorAttr = new GeneratedComponentAttribute();
+		colorAttr.setName("color");
+		colorAttr.setBasedOn("");
+		colorAttr.setDescription("What is the compiler's favourite color?");
+		colorAttr.setValue(null);
+		colorAttr.setUse(ISchemaAttribute.OPTIONAL);
+		attributes[2] = colorAttr;
+		*/
+		return attributes;
+    }
+    
+    
+    public String getParseControllerClassName(IProject project)
+    {
+        // Get the extension that represents the parser
+        IPluginModelBase pluginModel = pages[0].getPluginModel();
+        IExtensions extensionsThing = pluginModel.getExtensions();
+        IPluginExtension[] extensions = extensionsThing.getExtensions();
+        IPluginExtension parserExtension = null;
+        for (int i = 0; i < extensions.length; i++) {
+        	if(extensions[i].getPoint().equals("org.eclipse.uide.runtime.parser")) {
+        		parserExtension = extensions[i];
+        		break;
+        	}
+        }
+    	
+        // Get the plugin element that represents the class of the parser
+        PluginElement parserPluginElement = null;
+        if (parserExtension != null) {
+        	IPluginObject[] children = parserExtension.getChildren();
+        	for (int i = 0; i < children.length; i++) {
+        		if(children[i].getName().equals("parser")) {
+        			parserPluginElement = (PluginElement) children[i];
+        				break;
+        		}
+        	}
+        }
+        if (parserPluginElement == null) return null;
+ 
+        // Get the name of the parser package
+        IPluginAttribute parserClassAttribute = parserPluginElement.getAttribute("class");
+        String parserPackageName = parserClassAttribute.getValue();
+        parserPackageName = parserPackageName.substring(0, parserPackageName.lastIndexOf('.'));
+    	
+        // The ParseController class should be in that package, so look for it there
+
+        // Get the package (fragment) that contains the parser
+        IWorkspace workspace = project.getWorkspace();
+        IJavaModel javaModel = JavaCore.create(workspace.getRoot());
+        IJavaProject javaProject = javaModel.getJavaProject(project.getName());
+    	IPackageFragment parserPackage = null;
+        try {
+        	IPackageFragment[] packageFragments = javaProject.getPackageFragments();
+        	for (int i = 0; i < packageFragments.length; i++) {
+        		if (packageFragments[i].getElementName().equals(parserPackageName)) {
+        			parserPackage = packageFragments[i];
+        		}
+        	}
+        } catch (JavaModelException e) {
+        	System.err.println("NewCompiler.getParseControllerClassName(IProject):  JavaModelException getting parser package:  " +
+        			"\n\t" + e.getMessage() +
+        			"\n\tReturning null");
+        	return null;
+        }
+        if (parserPackage == null) return null;
+        
+        
+        // Check the classes in the parser package for one that represents an IParseController
+        // (assume there's just one)
+        try {
+            ICompilationUnit[] compilationUnits = parserPackage.getCompilationUnits();
+            typeSearch:  for (int i = 0; i < compilationUnits.length; i++) {
+            	ICompilationUnit unit = compilationUnits[i];
+            	// Get the type(s) declared by this compilation unit
+            	IType[] unitTypes = unit.getTypes();
+            	for (int j = 0; j < unitTypes.length; j++) {
+            		IType type = unitTypes[j];
+            		String[] superInterfaceNames = type.getSuperInterfaceNames();
+            		for (int k = 0; k < superInterfaceNames.length; k++) {
+            			if (superInterfaceNames[k].contains("IParseController")) {
+            				// Found it :-)
+            				return type.getElementName();
+            			}
+            		}
+            	}
+            }
+        } catch (JavaModelException e) {
+        	System.err.println("NewCompiler.getParseControllerClassName(IProject):  JavaModelException checking for IParseController:  " +
+        			"\n\t" + e.getMessage() +
+        			"\n\tReturning null");
+        	return null;
+        }
+        
+        // Didn't find it :-(
+        return null;
+    }
+    
+    
 }
