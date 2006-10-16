d626820684ff941346181e207ec797e21e7c37ab
Philippe Charles <pgc1@rice.edu>
2006-10-16 21:41:27 +0000

    New contentProposer wizard code and grammar template changes to support it.

--- src/org/eclipse/imp/wizards/NewContentProposer.java
+++ src/org/eclipse/imp/wizards/NewContentProposer.java
@@ -3,8 +3,14 @@
  */
 package org.eclipse.uide.wizards;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.uide.runtime.RuntimePlugin;
 
@@ -14,11 +20,41 @@ public class NewContentProposer extends CodeServiceWizard {
     }
 
     protected List getPluginDependencies() {
-        return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources",
-    	    "org.eclipse.uide.runtime" });
+        return Arrays.asList(new String[] { "org.eclipse.core.runtime",
+                                            "org.eclipse.core.resources",
+                                            "org.eclipse.uide.runtime" 
+                                          });
     }
 
-    public void generateCodeStubs(IProgressMonitor mon) {
+    public void generateCodeStubs(IProgressMonitor mon) throws CoreException {
     // TODO Auto-generated method stub
+        ExtensionPointWizardPage page= (ExtensionPointWizardPage) pages[0];
+        IProject project= page.getProject();
+        Map subs= getStandardSubstitutions();
+
+        subs.put("$PARSER_PKG$", fParserPackage);
+        subs.put("$AST_PKG$", fParserPackage + "." + Wizards.astDirectory);
+        subs.put("$AST_NODE$", Wizards.astNode);
+
+        // SMS 19 Jul 2006
+        // Added (or modified) following so as to accommodate
+        // values provided through wizard by user
+        
+        WizardPageField field = pages[0].getField("class");
+        String qualifiedClassName = field.fValue;
+        String className = qualifiedClassName.substring(qualifiedClassName.lastIndexOf('.')+1);
+        subs.remove("$CONTENT_PROPOSER_CLASS_NAME$");
+        subs.put("$CONTENT_PROPOSER_CLASS_NAME$", className);
+        
+        subs.remove("$PACKAGE_NAME$");
+        String packageName = qualifiedClassName.substring(0, qualifiedClassName.lastIndexOf('.'));
+        subs.put("$PACKAGE_NAME$", packageName);
+        String packageFolder = packageName.replace('.', File.separatorChar);
+ 
+        
+        //IFile outlinerSrc= createFileFromTemplate(fClassName + "Outliner.java", "outliner.tmpl", fPackageFolder, subs, project, mon);
+        IFile outlinerSrc= createFileFromTemplate(className      + ".java", "contentProposer.java", packageFolder, subs, project, mon);
+ 
+        editFile(mon, outlinerSrc);
     }
 }
---
+++ templates/contentProposer.java
@@ -0,0 +1,129 @@
+package $PACKAGE_NAME$;
+
+import $PARSER_PKG$.*;
+import $AST_PKG$.*;
+import lpg.lpgjavaruntime.*;
+
+import java.util.*;
+
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.uide.editor.IContentProposer;
+import org.eclipse.uide.editor.SourceProposal;
+import org.eclipse.uide.parser.Ast;
+import org.eclipse.uide.parser.IParseController;
+
+public class $CONTENT_PROPOSER_CLASS_NAME$ implements IContentProposer
+{
+    private ArrayList filterSymbols(List in_symbols, String prefix)
+    {
+        ArrayList symbols = new ArrayList();
+        for (int i = 0; i < in_symbols.size(); i++)
+        {
+                DemoParser.Symbol symbol = (DemoParser.Symbol) in_symbols.get(i);
+            String name = symbol.getName();
+            if (name.length() >= prefix.length() && prefix.equals(name.substring(0, prefix.length())))
+                symbols.add(symbol);
+        }
+
+        return symbols;
+    }
+
+    public ArrayList getVisibleVariables(DemoParser parser, ASTNode n) {
+        block b = null;
+        while (n != null) {
+            if (n instanceof block)
+            {
+                b = (block) n;
+                break;
+            }
+            else n = n.getParent();
+        }
+        
+        ArrayList result = new ArrayList();
+        HashSet set = new HashSet();
+        for (DemoParser.SymbolTable s = (b == null ? parser.getTopLevelSymbolTable() : b.getSymbolTable());
+             s != null;
+             s = s.getParent())
+        {
+            for (Enumeration e = s.keys(); e.hasMoreElements(); ) {
+                Object key = e.nextElement();
+                if (! set.contains(key))
+                {
+                    set.add(key);
+                    result.add(s.get(key));
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public ICompletionProposal[] getContentProposals(IParseController controller, int offset)
+    {
+        ArrayList list = new ArrayList();
+        //
+        //
+        //
+        DemoParser parser = (DemoParser) controller.getParser();
+        
+        //
+        // When the offset is in between two tokens (for example, on a white space or comment)
+        // the getTokenIndexAtCharacter in parse stream returns the negative index
+        // of the token preceding the offset. Here, we adjust this index to point instead
+        // to the token following the offset.
+        //
+        // Note that the controller also has a getTokenIndexAtCharacter and 
+        // getTokenAtCharacter. However, these methods won't work here because
+        // controller.getTokenAtCharacter(offset) returns null when the offset
+        // is not the offset of a valid token; and controller.getTokenAtCharacter(offset) returns
+        // the index of the token preceding the offset when the offset is not
+        // the offset of a valid token.
+        //
+        PrsStream prs_stream = parser.getParseStream(); 
+        int index = prs_stream.getTokenIndexAtCharacter(offset),
+            token_index = (index < 0 ? -(index - 1) : index);
+        IToken token = prs_stream.getIToken(token_index),
+               candidate = prs_stream.getIToken(prs_stream.getPrevious(token_index));
+
+        //
+        // If we are at an offset position immediately following an "identifier"
+        // candidate, then consider the candidate to be the token for which we need
+        // completion. If the candidate is not the left-hand side of an assignment,
+        // we move the candidate and its successor back one token as that still leaves
+        // us in the range of the assignment.
+        //
+        if (candidate.getKind() == DemoLexer.TK_IDENTIFIER &&
+            token.getKind() != DemoLexer.TK_ASSIGN &&
+            offset == candidate.getEndOffset() + 1) {
+            token = candidate;
+            candidate = prs_stream.getIToken(prs_stream.getPrevious(candidate.getTokenIndex()));
+        }
+        
+        String prefix = "";
+        if (token.getKind() == DemoLexer.TK_IDENTIFIER) {
+            if (offset >= token.getStartOffset() && offset <= token.getEndOffset() + 1)
+                prefix = token.toString().substring(0, offset - token.getStartOffset());
+        }
+        
+        DemoASTNodeLocator locator = new DemoASTNodeLocator(controller);
+        ASTNode node = (ASTNode) locator.findNode(controller.getCurrentAst(), candidate.getStartOffset(), candidate.getEndOffset()); // offset);
+        if (node != null) {
+            if (node.getParent() instanceof Iexpression ||
+                node.getParent() instanceof assignment ||
+                node.getParent() instanceof BadAssignment) {
+                ArrayList vars = filterSymbols(getVisibleVariables(parser, node), prefix);
+                for (int i = 0; i < vars.size(); i++) {
+                    DemoParser.Symbol symbol = (DemoParser.Symbol) vars.get(i);
+                    list.add(new SourceProposal(symbol.getType() + " " + symbol.getName(),
+                                                    symbol.getName(),
+                                                    prefix,
+                                                    offset));
+                }
+            }
+            else list.add(new SourceProposal("no info available", "", offset));
+        }
+        else list.add(new SourceProposal("no info available due to Syntax error(s)", "", offset));
+
+        return (ICompletionProposal[]) list.toArray(new ICompletionProposal[list.size()]);
+    }
+}
