63fd10ed4044876367868a0328284d638ec57264
Stanley M. Sutton <suttons@us.ibm.com>
2007-01-11 19:26:24 +0000

    Added an ITextViewer parameter to the method getContentProposals(..) to keep it consistent with a previous change to the method specification in IContentProposer.

--- templates/contentProposer.java
+++ templates/contentProposer.java
@@ -7,10 +7,12 @@ import lpg.lpgjavaruntime.*;
 import java.util.*;
 
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.uide.editor.IContentProposer;
 import org.eclipse.uide.editor.SourceProposal;
 import org.eclipse.uide.parser.Ast;
 import org.eclipse.uide.parser.IParseController;
+
 
 public class $CONTENT_PROPOSER_CLASS_NAME$ implements IContentProposer
 {
@@ -65,7 +67,24 @@ public class $CONTENT_PROPOSER_CLASS_NAME$ implements IContentProposer
         return "";
     }
 
-    public ICompletionProposal[] getContentProposals(IParseController controller, int offset)
+
+    /**
+     * Returns an array of content proposals applicable relative to the AST of the given
+     * parse controller at the given position.
+     * 
+     * (The provided ITextViewer is not used in the default implementation provided here
+     * but but is stipulated by the IContentProposer interface for purposes such as accessing
+     * the IDocument for which content proposals are sought.)
+     * 
+     * @param controller	A parse controller from which the AST of the document being edited
+     * 						can be obtained
+     * @param int			The offset for which content proposals are sought
+     * @param viewer		The viewer in which the document represented by the AST in the given
+     * 						parse controller is being displayed (may be null for some implementations)
+     * @return				An array of completion proposals applicable relative to the AST of the given
+     * 						parse controller at the given position
+     */
+    public ICompletionProposal[] getContentProposals(IParseController controller, int offset, ITextViewer viewer)
     {
         // START_HERE           
         ArrayList list = new ArrayList(); // a list of proposals.
