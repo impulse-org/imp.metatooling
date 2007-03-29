7e31fef699941bbc7f1781fa4eb7cdaf1a6680a7
Stanley M. Sutton <suttons@us.ibm.com>
2007-03-29 17:35:01 +0000

    Several changes relating mainly to increased parameterization of names:  Parameterized plugin package name, plugin class name, and plugin id; used these where appropriate in the template.
    Also removed examples of the use of the old preferences mechanism.

--- templates/plugin.java
+++ templates/plugin.java
@@ -1,40 +1,51 @@
-package $PACKAGE_NAME$;
+package $PLUGIN_PACKAGE$;
+//SMS 28 Mar 2007:  plugin package now a separate parameter
 
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.uide.runtime.SAFARIPluginBase;
-import $PACKAGE_NAME$.preferences.$CLASS_NAME_PREFIX$PreferenceCache;
-import $PACKAGE_NAME$.preferences.$CLASS_NAME_PREFIX$PreferenceConstants;
+//import $PACKAGE_NAME$.preferences.$CLASS_NAME_PREFIX$PreferenceCache;
+//import $PACKAGE_NAME$.preferences.$CLASS_NAME_PREFIX$PreferenceConstants;
+// SMS 27 Mar 2007:  Taking out preference cache elements as no longer useful
+//import $PLUGIN_PACKAGE$.preferences.$CLASS_NAME_PREFIX$PreferenceCache;
+import $PLUGIN_PACKAGE$.preferences.$CLASS_NAME_PREFIX$PreferenceConstants;
 import org.osgi.framework.BundleContext;
 
-public class $CLASS_NAME_PREFIX$Plugin extends SAFARIPluginBase {
-    public static final String kPluginID= "$LANG_NAME$";
+// SMS 28 Mar 2007:  plugin class name now totally parameterized
+// in all uses here
+
+public class $PLUGIN_CLASS$ extends SAFARIPluginBase {
+	// SMS 28 Mar 2007:  now assign actual plugin id from parameter
+    public static final String kPluginID= "$PLUGIN_ID$";
 
     /**
      * The unique instance of this plugin class
      */
-    protected static $CLASS_NAME_PREFIX$Plugin sPlugin;
+    protected static $PLUGIN_CLASS$ sPlugin;
 
-    public static $CLASS_NAME_PREFIX$Plugin getInstance() {
+    public static $PLUGIN_CLASS$ getInstance() {
         return sPlugin;
     }
 
-    public $CLASS_NAME_PREFIX$Plugin() {
-	super();
-	sPlugin= this;
+    public $PLUGIN_CLASS$() {
+    	super();
+    	sPlugin= this;
     }
 
     public void start(BundleContext context) throws Exception {
         super.start(context);
 
-	// Initialize the Preferences fields with the preference store data.
-	IPreferenceStore prefStore= getPreferenceStore();
-
-	$CLASS_NAME_PREFIX$PreferenceCache.builderEmitMessages= prefStore.getBoolean($CLASS_NAME_PREFIX$PreferenceConstants.P_EMIT_MESSAGES);
-
-	fEmitInfoMessages= $CLASS_NAME_PREFIX$PreferenceCache.builderEmitMessages;
+        // SMS 29 Mar 2007:  outdated use of preference store;
+        // TODO  Replace with use of preferences service
+		// Initialize the Preferences fields with the preference store data.
+		IPreferenceStore prefStore= getPreferenceStore();
+	
+		// SMS 27 Mar 2007
+		// Deleted examples of use of preferences cache, since preference cache
+		// has been deleted.
+		// TODO  Add some examples using new preferences service
     }
 
     public String getID() {
-	return kPluginID;
+    	return kPluginID;
     }
 }
