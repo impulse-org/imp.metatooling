9c2657ecef0db47175fcc4c9acdfd1d85436b742
Stanley M. Sutton <suttons@us.ibm.com>
2007-04-06 20:16:18 +0000

    Modified to implement IOutlineImage, so as to facilitate use by outliners, and changed substitution parameter for the plugin with the image icons from $LANG_NAME$ to $PLUGIN_ID$.

--- templates/images.java
+++ templates/images.java
@@ -3,11 +3,39 @@ package $PACKAGE_NAME$;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.uide.editor.IOutlineImage;
 
-public class $CLASS_NAME_PREFIX$Images {
+
+public class $CLASS_NAME_PREFIX$Images implements IOutlineImage
+{
+	private $CLASS_NAME_PREFIX$Images() { }
+	
+	private static IOutlineImage image = null;
+	
+	public static IOutlineImage get$CLASS_NAME_PREFIX$Images() {
+		if (image == null) {
+			image = new $CLASS_NAME_PREFIX$Images();
+		}
+		return image;
+	}
+
 	public static final String IMAGE_ROOT= "icons";
 
-	public static ImageDescriptor OUTLINE_ITEM_DESC= AbstractUIPlugin.imageDescriptorFromPlugin("$LANG_NAME$", IMAGE_ROOT + "/outline_item.gif");
-
+	public static ImageDescriptor OUTLINE_ITEM_DESC= AbstractUIPlugin.imageDescriptorFromPlugin(
+		"$PLUGIN_ID$", IMAGE_ROOT + "/outline_item.gif");
 	public static Image OUTLINE_ITEM_IMAGE= OUTLINE_ITEM_DESC.createImage();
+
+	
+	public String getImageRoot() {
+		return IMAGE_ROOT;
+	}
+
+	public ImageDescriptor getOutlineItemDesc() {
+		return OUTLINE_ITEM_DESC;
+	}
+
+	public Image getOutlineItemImage() {
+		return OUTLINE_ITEM_IMAGE;
+	}
+	
 }
