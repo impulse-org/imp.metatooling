9b1b3986f872dc23057f6d9551a45683bf4549b8
Stanley M. Sutton <suttons@us.ibm.com>
2007-06-19 18:52:51 +0000

    Added constant field kLanguageName and cleaned up comments.

--- templates/plugin.java
+++ templates/plugin.java
@@ -1,5 +1,4 @@
 package $PLUGIN_PACKAGE$;
-//SMS 28 Mar 2007:  plugin package now a separate parameter
 
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.uide.runtime.SAFARIPluginBase;
@@ -7,13 +6,20 @@ import org.eclipse.uide.runtime.SAFARIPluginBase;
 //import $PACKAGE_NAME$.preferences.$CLASS_NAME_PREFIX$PreferenceConstants;
 import org.osgi.framework.BundleContext;
 
-// SMS 28 Mar 2007:  plugin class name now totally parameterized
-// in all uses here
+/*
+ * SMS 27 Mar 2007:  Deleted creation of preferences cache (now obsolete)
+ * SMS 28 Mar 2007:
+ * 	- Plugin class name now totally parameterized
+ *  - Plugin package made a separate parameter
+ * SMS 19 Jun 2007:  Added kLanguageName (may be used by later
+ * 	updates to the template)
+ */ 
 
 public class $PLUGIN_CLASS$ extends SAFARIPluginBase {
-	// SMS 28 Mar 2007:  now assign actual plugin id from parameter
-    public static final String kPluginID= "$PLUGIN_ID$";
 
+    public static final String kPluginID= "$PLUGIN_ID$";
+    public static final String kLanguageName = "$LANG_NAME$";
+    
     /**
      * The unique instance of this plugin class
      */
@@ -31,18 +37,13 @@ public class $PLUGIN_CLASS$ extends SAFARIPluginBase {
     public void start(BundleContext context) throws Exception {
         super.start(context);
 
-        // SMS 29 Mar 2007:  outdated use of preference store;
         // TODO  Replace with use of preferences service
 		// Initialize the Preferences fields with the preference store data.
 		IPreferenceStore prefStore= getPreferenceStore();
-	
-		// SMS 27 Mar 2007
-		// Deleted examples of use of preferences cache, since preference cache
-		// has been deleted.
-		// TODO  Add some examples using new preferences service
     }
 
     public String getID() {
     	return kPluginID;
-    }
+    }  
+    
 }
