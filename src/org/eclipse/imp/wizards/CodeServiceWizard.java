8837ca871975bbb0c3a3487a3f08180fcf55bdf8
Stanley M. Sutton <suttons@us.ibm.com>
2007-07-27 15:06:19 +0000

    Migrated method getASTInformation() to ExtensionPointUtils and called it there.  Cleaned up imports.

--- src/org/eclipse/imp/wizards/CodeServiceWizard.java
+++ src/org/eclipse/imp/wizards/CodeServiceWizard.java
@@ -8,14 +8,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.pde.core.plugin.IExtensions;
-import org.eclipse.pde.core.plugin.IPluginAttribute;
-import org.eclipse.pde.core.plugin.IPluginExtension;
 import org.eclipse.pde.core.plugin.IPluginModel;
-import org.eclipse.pde.core.plugin.IPluginModelBase;
-import org.eclipse.pde.core.plugin.IPluginObject;
 import org.eclipse.pde.internal.core.bundle.WorkspaceBundleModel;
-import org.eclipse.pde.internal.core.plugin.PluginElement;
+import org.eclipse.uide.utils.ExtensionPointUtils;
 	
 /**
  * An ExtensionPointWizard that also generates source code from one or more template files.
@@ -233,7 +228,7 @@ public abstract class CodeServiceWizard extends ExtensionPointWizard {
         // compile, but if we don't provide any values then it won't compile in
         // any case--specifically because substitutions for these parameters will
         // not have been made)
-        result = getASTInformation();
+        result = ExtensionPointUtils.getASTInformation((IPluginModel)pages[0].getPluginModel(), fProject);
         
         // continuing with original:
         result.put("$LANG_NAME$", fLanguageName);
@@ -250,105 +245,6 @@ public abstract class CodeServiceWizard extends ExtensionPointWizard {
         result.put("$PLUGIN_CLASS$", getPluginClassName(null, null));
         result.put("$PLUGIN_ID$", getPluginID(null, null));
 
-        return result;
-    }
-    
-    
-    /**
-     * Return a Map containing the the names of the AST package and class
-     * bound to "well-known" symbols, "$AST_PACKAGE$" and "$AST_CLASS$", 
-     * respectively.
-     * 
-     * WARNING:  The names returned are currently the DEFAULT names (which
-     * should be the most commonly occurring but which may not be appropriate
-     * in general).
-     * 
-     * The actual values for the AST package and class are generated in the
-     * NewParser wizard but are not (yet) stored anywhere for reference by
-     * other wizards.  There is at least one other wizard, the NewFoldingUpdater
-     * wizard, which does need these names to complete a template.  In order
-     * to make available some reasonable values for these names, this method
-     * recomputes the names using the same assumptions as are used for the
-     * default case in the NewParser wizard.
-     * 
-     * TODO:  Provide a means for (more) persistently maintaining the names
-     * of hte AST package and class in such a way that they can become part
-     * of the "standard substitutions."  (ALTERNATIVELY:  the class could just
-     * be obtained in wizards where needed, in which case it need not be part
-     * of the standard substitutions.)
-     * 
-     * @return	A Map that contains two valuse:  the name of the package that
-     * 			contains the AST class, and the name of the AST class.
-     * 			These values keyed, respectively, by the symbols "$AST_PACKAGE$"
-     * 			and "$AST_NODE$".
-     * 
-     * Updates:  Stan Sutton, 9 Aug 2006
-     * 			Changed return from $AST_CLASS$ to $AST_NODE$ since the latter
-     * 			is the symbol more commonly used (and the one on which I will
-     * 			try to standardize)
-     * 
-     * @author	Stan Sutton
-     * @since	17 May 2006
-     */
-    protected Map<String,String> getASTInformation()
-    {
-    	Map<String,String> result = new HashMap();
-    	
-        // Get the extension that represents the parser
-        IPluginModelBase pluginModel = pages[0].getPluginModel();
-        IExtensions extensionsThing = pluginModel.getExtensions();
-        IPluginExtension[] extensions = extensionsThing.getExtensions();
-        IPluginExtension parserExtension = null;
-        for (int i = 0; i < extensions.length; i++) {
-        	if(extensions[i].getPoint().equals("org.eclipse.uide.runtime.parser")) {
-        		parserExtension = extensions[i];
-        		break;
-        	}
-        }
-
-        // Get the plugin element that represents the class of the parser
-        PluginElement parserPluginElement = null;
-        if (parserExtension != null) {
-        	IPluginObject[] children = parserExtension.getChildren();
-        	for (int i = 0; i < children.length; i++) {
-        		String childName = children[i].getName();
-        		if(childName != null &&
-        		   (childName.equals("parser") || childName.equals("parserWrapper")))
-        		{
-        			parserPluginElement = (PluginElement) children[i];
-        			break;
-        		}
-        	}
-        }
-        if (parserPluginElement == null) return result;
-
-        /*
-        IPluginAttribute astPackageAttribute = parserPluginElement.getAttribute("class");
-        String astPackageName = astPackageAttribute.getValue();
-        astPackageName = astPackageName.substring(0, astPackageName.lastIndexOf('.'));
-        astPackageName += ".Ast";
-        String astClassName = "ASTNode";
-        */
-        
-        // Get the names of the parser package, AST package, and AST (node) class name
-        
-        IPluginAttribute parserClassAttribute = parserPluginElement.getAttribute("class");
-        String parserPackageName = parserClassAttribute.getValue();
-        parserPackageName = parserPackageName.substring(0, parserPackageName.lastIndexOf('.'));
-        // ASSUME that the AST package name is the parser package name extended 
-        // with ".Ast" (this is the default when auto-generated)
-        String astPackageName = parserPackageName + ".Ast";
-        // Just assume this is true
-        // TBD:  check whether this exists (or put the info somewhere from
-        // where it can be retrieved here)
-        String astClassName = "ASTNode";
-        
-        // Save these values in the substitutions map
-        result.put("$PROJ_NAME$", fProjectName);
-        result.put("$PARSER_PACKAGE$", parserPackageName);
-        result.put("$AST_PACKAGE$", astPackageName);
-        result.put("$AST_NODE$", astClassName);
-        
         return result;
     }
     
