92891f203d4039e8c2c512c4994a3af0ed561a0a
Stanley M. Sutton <suttons@us.ibm.com>
2007-11-26 20:48:49 +0000

    Modified createLanguageFieldForComponent(..) to do nothing, since that method assumes that a language has been defined for the project, when actually this wizard is about to create that definition.
    Overrode methods getSelectionValidatorForProjects
    (..) and addFilterToDialog(..) so as to provide validators and filters for plugin projects rather than for IDE projects (which is the default behavior).

--- src/org/eclipse/imp/wizards/NewLanguageWizardPage.java
+++ src/org/eclipse/imp/wizards/NewLanguageWizardPage.java
@@ -1,6 +1,12 @@
 package org.eclipse.imp.wizards;
 
+import org.eclipse.imp.ui.dialogs.ListSelectionDialog;
+import org.eclipse.imp.ui.dialogs.filters.ViewerFilterForIDEProjects;
+import org.eclipse.imp.ui.dialogs.filters.ViewerFilterForPluginProjects;
+import org.eclipse.imp.ui.dialogs.validators.SelectionValidatorForPluginProjects;
+import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.dialogs.ISelectionValidator;
 
 /**
  * A wizard page for the New Language wizard.  Not especially different
@@ -10,7 +16,7 @@ import org.eclipse.swt.widgets.Composite;
  * which is appropriate for all wizards with the field except this one.)
  * 
  * @author sutton (Stan Sutton, suttons@us.ibm.com)
- * @since 2007 11 13
+ * @since 20071113
  *
  */
 public class NewLanguageWizardPage extends ExtensionPointWizardPage {
@@ -31,10 +37,48 @@ public class NewLanguageWizardPage extends ExtensionPointWizardPage {
         super(owner, pageNumber, totalPages, pluginID, pointID, isOptional);
     }
 	
+    /**
+     * Overrides the default version of this method so as to do nothing, i.e.,
+     * so as to not create a language field.
+     * 
+     * This method is called "automatically" for all IMP wizard pages.  By default
+     * it creates a language field that shows the langauge associated with the
+     * selected project.  This is apporpriate for almost all IMP wizards.  The
+     * NewLanguage wizard is an exception, since this is the wizard through which
+     * the language is associated with the project in the first place.  The wizard
+     * page does need a language field (for the naming of the new language), but the
+     * field is defined as an attribute in the extension schema and will be created
+     * by the mechanism that creates fields for all extension attributes.
+     * 
+     * @see org.eclipse.imp.wizards.IMPWizardPage#createLanguageFieldForComponent(org.eclipse.swt.widgets.Composite, java.lang.String)
+     */
     protected void createLanguageFieldForComponent(Composite parent, String componentID) {
-    	super.createLanguageFieldForComponent(parent, componentID);
-    	fLanguageText.setEnabled(true);
+    	// nichts
     }
-	
+    
+    /**
+     * Overrides the default version of this method so as to add a viewer filter
+     * for plug-in projects rather than for IDE projects (as in the default case).
+     * The language must be defined in a plug-in project, but that project does
+     * not become an IDE project until after this wizard is completed.
+     * 
+     * @see org.eclipse.imp.wizards.IMPWizardPage#addFilterToDialog(org.eclipse.imp.ui.dialogs.ListSelectionDialog)
+     */
+    protected void addFilterToDialog(ListSelectionDialog dialog) {
+        dialog.addFilter(new ViewerFilterForPluginProjects());
+    }
+    
+    /**
+     * Overrides the default version of this method so as to add a selection validator
+     * for plug-in projects rather than for IDE projects (as in the default case).
+     * The language must be defined in a plug-in project, but that project does
+     * not become an IDE project until after this wizard is completed.
+     * 
+     * @see org.eclipse.imp.wizards.IMPWizardPage#getSelectionValidatorForProjects()
+     */
+	protected ISelectionValidator getSelectionValidatorForProjects() {
+		return new SelectionValidatorForPluginProjects();
+	}
+    
 	
 }
