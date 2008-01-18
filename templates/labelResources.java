853e8ea3c9eb0b645f5856cb61b7ab789272d52f
Stanley M. Sutton <suttons@us.ibm.com>
2008-01-18 00:20:09 +0000

    Template for a file to define String resources for an IDE plugin, in particular constant names for default images.

---
+++ templates/labelResources.java
@@ -0,0 +1,19 @@
+package $PLUGIN_PACKAGE$;
+
+public interface I$CLASS_NAME_PREFIX$Resources {
+
+	// Definitions for label provider
+	
+	public static final String $CLASS_NAME_PREFIX_UPPER$_DEFAULT_IMAGE = "$CLASS_NAME_PREFIX_LOWER$_default_image";
+	
+	public static final String $CLASS_NAME_PREFIX_UPPER$_DEFAULT_OUTLINE_ITEM = "$CLASS_NAME_PREFIX_LOWER$_default_outline_item";
+	
+	public static final String $CLASS_NAME_PREFIX_UPPER$_FILE = "$CLASS_NAME_PREFIX_LOWER$_file";
+	
+	public static final String $CLASS_NAME_PREFIX_UPPER$_FILE_WARNING = "$CLASS_NAME_PREFIX_LOWER$_file_warning";
+	
+	public static final String $CLASS_NAME_PREFIX_UPPER$_FILE_ERROR = "$CLASS_NAME_PREFIX_LOWER$_file_error";
+
+	// Definitions for label provider end
+	
+}
