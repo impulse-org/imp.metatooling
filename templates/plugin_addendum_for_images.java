84192609b46f1d98e45e694bff2b633e132a1a5f
Stanley M. Sutton <suttons@us.ibm.com>
2008-01-22 15:09:28 +0000

    Updated to use qualified type names so as to avoid having to add important statements that might be needed.

--- templates/plugin_addendum_for_images.java
+++ templates/plugin_addendum_for_images.java
@@ -1,11 +1,12 @@
    
 	// Definitions for image management
 
-	public static final org.eclipse.core.runtime.IPath ICONS_PATH= new Path("icons/"); //$NON-NLS-1$
+	public static final org.eclipse.core.runtime.IPath ICONS_PATH=
+		new org.eclipse.core.runtime.Path("icons/"); //$NON-NLS-1$("icons/"); //$NON-NLS-1$
 
-    protected void initializeImageRegistry(ImageRegistry reg) {
-        IPath path= ICONS_PATH.append("$CLASS_NAME_PREFIX_LOWER$_default_image.gif");//$NON-NLS-1$
-        ImageDescriptor imageDescriptor= createImageDescriptor(getInstance().getBundle(), path);
+    protected void initializeImageRegistry(org.eclipse.jface.resource.ImageRegistry reg) {
+    	org.eclipse.core.runtime.IPath path= ICONS_PATH.append("$CLASS_NAME_PREFIX_LOWER$_default_image.gif");//$NON-NLS-1$
+    	org.eclipse.jface.resource.ImageDescriptor imageDescriptor= createImageDescriptor(getInstance().getBundle(), path);
         reg.put(I$CLASS_NAME_PREFIX$Resources.$CLASS_NAME_PREFIX_UPPER$_DEFAULT_IMAGE, imageDescriptor);
 
         path= ICONS_PATH.append("$CLASS_NAME_PREFIX_LOWER$_default_outline_item.gif");//$NON-NLS-1$
@@ -25,10 +26,13 @@
         reg.put(I$CLASS_NAME_PREFIX$Resources.$CLASS_NAME_PREFIX_UPPER$_FILE_ERROR, imageDescriptor);
     }
 
-    public static ImageDescriptor createImageDescriptor(Bundle bundle, IPath path) {
-        java.net.URL url= FileLocator.find(bundle, path, null);
+    public static org.eclipse.jface.resource.ImageDescriptor createImageDescriptor(
+    		org.osgi.framework.Bundle bundle, 
+    		org.eclipse.core.runtime.IPath path)
+    {
+        java.net.URL url= org.eclipse.core.runtime.FileLocator.find(bundle, path, null);
         if (url != null) {
-            return ImageDescriptor.createFromURL(url);
+            return org.eclipse.jface.resource.ImageDescriptor.createFromURL(url);
         }
         return null;
     }
