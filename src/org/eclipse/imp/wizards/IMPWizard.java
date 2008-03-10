6ca1eb43e3828766b93180a0ade8ef5cf6524f3f
Stanley M. Sutton <suttons@us.ibm.com>
2008-03-10 20:26:16 +0000

    Added versions of getProjectSourceLocation(..) hoisted from subclasses.

--- src/org/eclipse/imp/wizards/IMPWizard.java
+++ src/org/eclipse/imp/wizards/IMPWizard.java
@@ -1,5 +1,13 @@
 package org.eclipse.imp.wizards;
 
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.internal.core.JavaModel;
+import org.eclipse.jdt.internal.core.JavaModelManager;
 import org.eclipse.jface.wizard.Wizard;
 
 public class IMPWizard extends Wizard {
@@ -17,5 +25,46 @@ public class IMPWizard extends Wizard {
     public void setPage(int page) {
     	currentPage= page;
     }
+    
+    
+    // SMS 13 Apr 2007
+    // A step toward relaxing assumptions about the location
+    // of source files within the project
+    public static String getProjectSourceLocation() {
+    		return "src/";
+    }
+
 	
+    
+    public static String getProjectSourceLocation(IProject project) {
+		try {
+			if (project == null)
+				return null;
+			JavaModel jm = JavaModelManager.getJavaModelManager().getJavaModel();
+			IJavaProject jp = jm.getJavaProject(project);
+			if (jp == null)
+				return null;
+			else {
+				IPackageFragmentRoot[] roots = jp.getPackageFragmentRoots();
+				for (int i = 0; i < roots.length; i++) {
+					if (roots[i].getCorrespondingResource() instanceof IFolder) {
+						IPath lcnPath = roots[i].getPath();
+						lcnPath = lcnPath.removeFirstSegments(1);
+						String lcn = lcnPath.toString();
+						if (lcn.startsWith("/"))
+							lcn = lcn.substring(1);
+						if (!lcn.endsWith("/"))
+							lcn = lcn + "/";
+						return lcn;
+					}
+				}
+			}
+		} catch (JavaModelException e) {
+			
+		}
+		return null;
+    }
+    
+    
+    
 }
