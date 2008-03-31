959ef38d58dce5b96bf0a31f8836385f5756b2b1
Stanley M. Sutton <suttons@us.ibm.com>
2008-03-31 21:33:55 +0000

    Expanded getDocumentation(..) method to include an example in which text is returned based on an ASTNode type, not just on a token kind.

--- templates/documentationProvider.java
+++ templates/documentationProvider.java
@@ -3,11 +3,12 @@ package $DOCUMENTATION_PROVIDER_PACKAGE_NAME$;
 
 import org.eclipse.imp.language.ILanguageService;
 import org.eclipse.imp.parser.IParseController;
+import org.eclipse.imp.parser.SimpleLPGParseController;
 import org.eclipse.imp.services.IDocumentationProvider;
 
 import $PARSER_PKG$.Ast.*;
 
-import $PARSER_PACKAGE$.$LEXER_CLASS_NAME$;
+import $PARSER_PKG$.$LEXER_CLASS_NAME$;
 
 
 public class $DOCUMENTATION_PROVIDER_CLASS_NAME$ implements IDocumentationProvider, ILanguageService {
@@ -20,14 +21,24 @@ public class $DOCUMENTATION_PROVIDER_CLASS_NAME$ implements IDocumentationProvid
             return null;
         
         if (node instanceof ASTNode) {
-        	int tokenKind = ((ASTNode) node).getRightIToken().getKind();
-        	
-        	switch (tokenKind) {
         	
         	// START HERE
-        	// Create a case for each kind of token for which you want
-        	// to provide help text and return the text corresponding
-        	// to that token kind (such as in the following examples)
+        	// Create a case for each kind of node or token for which you
+        	// want to provide help text and return the text corresponding
+        	// to that entity (such as in the following examples)
+        	
+        	// Address node types of interest, which may represent multiple tokens
+			if (node instanceof declaration) {
+				declaration decl = (declaration) node;
+				return decl.getprimitiveType().toString() +
+					   " " + decl.getidentifier().toString();
+			}
+        	
+			// Address node types that can be treated as individual tokens
+        	int tokenKind = getTokenKindForNode((ASTNode)node);
+        	
+        	switch (tokenKind) {
+
         	case $LEXER_CLASS_NAME$.TK_IDENTIFIER:
         		return "This is an identifier";
         	case $LEXER_CLASS_NAME$.TK_NUMBER:
@@ -42,8 +53,15 @@ public class $DOCUMENTATION_PROVIDER_CLASS_NAME$ implements IDocumentationProvid
         return null;
     }
 
-
+    public int getTokenKindForNode(ASTNode node) {
+    	// If you want some token for a node
+    	// other than the right token, compute
+    	// that here ...
+    	return node.getRightIToken().getKind();
+    }
+    
+    
     public static String getSubstring(IParseController parseController, int start, int end) {
-        return new String(parseController.getLexer().getLexStream().getInputChars(), start, end-start+1);
+        return new String(((SimpleLPGParseController) parseController).getLexer().getLexStream().getInputChars(), start, end-start+1);
     }
 }
