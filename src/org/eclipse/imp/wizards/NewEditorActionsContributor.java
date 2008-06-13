180d44da683740ca91bfc741fa25b23c33d769a4
Jurgen J. Vinju <jurgen.vinju@cwi.nl>
2008-06-13 09:12:55 +0000

    * added wizard for editorActionsContributor

---
+++ src/org/eclipse/imp/wizards/NewEditorActionsContributor.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+* Copyright (c) 2007 IBM Corporation.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+
+*******************************************************************************/
+
+/**
+ * 
+ */
+package org.eclipse.imp.wizards;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.imp.runtime.RuntimePlugin;
+
+public class NewEditorActionsContributor extends CodeServiceWizard {
+	
+	
+    public void addPages() {
+	addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.IMP_RUNTIME, "editorActionContributions"), });
+    }
+
+    protected List getPluginDependencies() {
+		return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources",
+			"org.eclipse.imp.runtime", "org.eclipse.ui", "org.eclipse.jface.text"});
+    }
+    
+    
+    public void generateCodeStubs(IProgressMonitor mon) throws CoreException
+    {
+		Map subs= getStandardSubstitutions();
+		
+		subs.remove("$ACTION_CONTRIBUTOR_CLASS_NAME$");
+		subs.put("$ACTION_CONTRIBUTOR_CLASS_NAME$", fFullClassName);
+	
+		subs.remove("$PACKAGE_NAME$");
+		subs.put("$PACKAGE_NAME$", fPackageName);
+	
+		String editorActionsContributor = "editorActionsContributor.java";
+		IFile src= WizardUtilities.createFileFromTemplate(fFullClassName + ".java", editorActionsContributor, fPackageFolder, getProjectSourceLocation(fProject), subs, fProject, mon);
+	
+		editFile(mon, src);
+    }
+    
+    
+}
---
+++ templates/editorActionsContributor.java
@@ -0,0 +1,35 @@
+package $PACKAGE_NAME$;
+
+import org.eclipse.imp.editor.UniversalEditor;
+import org.eclipse.imp.services.ILanguageActionsContributor;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IStatusLineManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
+
+public class $ACTION_CONTRIBUTOR_CLASS_NAME$ implements ILanguageActionsContributor {
+	
+	public void contributeToEditorMenu(final UniversalEditor editor,
+			IMenuManager menuManager) {
+		IMenuManager languageMenu = new MenuManager("$LANGUAGE_NAME$");
+		menuManager.add(languageMenu);
+		languageMenu.add(new Action("Example") { 
+			// TODO implement run method here
+		});
+	}
+
+	public void contributeToMenuBar(UniversalEditor editor, IMenuManager menu) {
+		// TODO implement contributions and add them to the menu
+	}
+
+	public void contributeToStatusLine(final UniversalEditor editor,
+			IStatusLineManager statusLineManager) {
+		// TODO add ControlContribution objects to the statusLineManager
+	}
+
+	public void contributeToToolBar(UniversalEditor editor,
+			IToolBarManager toolbarManager) {
+		// add ControlContribution objects to the toolbarManager
+	}
+}
