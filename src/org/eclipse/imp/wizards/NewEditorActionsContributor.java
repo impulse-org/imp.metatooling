12d41ab654f97fff74cc021a05ea6345955026ad
Stanley M. Sutton <suttons@us.ibm.com>
2008-11-06 18:31:42 +0000

    Updated copyright date, removed empty header comment.

--- src/org/eclipse/imp/wizards/NewEditorActionsContributor.java
+++ src/org/eclipse/imp/wizards/NewEditorActionsContributor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2007 IBM Corporation.
+* Copyright (c) 2008 IBM Corporation.
 * All rights reserved. This program and the accompanying materials
 * are made available under the terms of the Eclipse Public License v1.0
 * which accompanies this distribution, and is available at
@@ -10,9 +10,6 @@
 
 *******************************************************************************/
 
-/**
- * 
- */
 package org.eclipse.imp.wizards;
 
 import java.util.Arrays;
