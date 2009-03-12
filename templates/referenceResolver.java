f55f21f683abe7516b1ff46168120312db1e6050
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-03-12 17:20:46 +0000

    Removed redundant implements clause on ILanguageService (IReferenceResolver already extends it). Fixed a few comment typos.

--- templates/referenceResolver.java
+++ templates/referenceResolver.java
@@ -10,29 +10,29 @@ import $PARSER_PKG$.$CLASS_NAME_PREFIX$Parser;
 import $PARSER_PKG$.Ast.*;
 
 
-public class $REFERENCE_RESOLVER_CLASS_NAME$ implements IReferenceResolver, ILanguageService {
+public class $REFERENCE_RESOLVER_CLASS_NAME$ implements IReferenceResolver {
 
     public $REFERENCE_RESOLVER_CLASS_NAME$ () {
     }
 
     /**
-      * Get the text associated with a given node for use in a link
+      * Get the text associated with the given node for use in a link
       * from (or to) that node
       */
     public String getLinkText(Object node) {
         // TODO:  Replace the call to super.getLinkText(..) with an implementation
-        // suitable to you language and link types
+        // suitable to your language and link types
         return node.toString();
     }
         
     /**
-      * Get the target for a given source node in the AST represented by a
+      * Get the target for the given source node in the AST produced by the
       * given Parse Controller.
       */
     public Object getLinkTarget(Object node, IParseController controller) {
         // START_HERE
         // Replace the given implementation with an implementation
-        // that is suitable to you language and link types
+        // that is suitable to your language and reference types
 		
         // NOTE:  The code shown in this method body works with the
         // example grammar used in the IMP language-service templates.
