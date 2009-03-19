b51f42178a6e4f535a81304941643573d1f4b6a9
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-03-19 15:41:04 +0000

    Moved ctor up front before other methods. Commented out string-/comment-related code.

--- templates/colorer_simple.java
+++ templates/colorer_simple.java
@@ -14,9 +14,27 @@ import $PARSER_PKG$.$CLASS_NAME_PREFIX$Parsersym;
 import lpg.runtime.IToken;
 
 public class $COLORER_CLASS_NAME$ extends TokenColorerBase implements $CLASS_NAME_PREFIX$Parsersym, ITokenColorer {
+    protected final TextAttribute doubleAttribute, identifierAttribute, keywordAttribute, numberAttribute;
+//  protected final TextAttribute commentAttribute, stringAttribute;
 
-    TextAttribute commentAttribute, keywordAttribute, stringAttribute, numberAttribute, doubleAttribute, identifierAttribute;
-
+    public $COLORER_CLASS_NAME$() {
+        super();
+        // TODO Define text attributes for the various token types that will have their text colored
+        //
+        // NOTE: Colors (i.e., instances of org.eclipse.swt.graphics.Color) are system resources
+        // and are limited in number.  THEREFORE, it is good practice to reuse existing system Colors
+        // or to allocate a fixed set of new Colors and reuse those.  If new Colors are instantiated
+        // beyond the bounds of your system capacity then your Eclipse invocation may cease to function
+        // properly or at all.
+        Display display = Display.getDefault();
+        doubleAttribute = new TextAttribute(display.getSystemColor(SWT.COLOR_DARK_GREEN), null, SWT.BOLD);
+        identifierAttribute = new TextAttribute(display.getSystemColor(SWT.COLOR_BLACK), null, SWT.NORMAL);
+        keywordAttribute = new TextAttribute(display.getSystemColor(SWT.COLOR_DARK_MAGENTA), null, SWT.BOLD);
+        numberAttribute = new TextAttribute(display.getSystemColor(SWT.COLOR_DARK_YELLOW), null, SWT.BOLD);
+//      commentAttribute = new TextAttribute(display.getSystemColor(SWT.COLOR_DARK_RED), null, SWT.ITALIC);
+//      stringAttribute = new TextAttribute(display.getSystemColor(SWT.COLOR_DARK_BLUE), null, SWT.BOLD);
+    }
+    
 	public TextAttribute getColoring(IParseController controller, Object o) {
 		if (o == null)
 			return null;
@@ -32,39 +50,18 @@ public class $COLORER_CLASS_NAME$ extends TokenColorerBase implements $CLASS_NAM
             return numberAttribute;
         case TK_DoubleLiteral:
             return doubleAttribute;
-//          case TK_StringLiteral:
-//               return stringAttribute;
-		case TK_SINGLE_LINE_COMMENT:
-			return commentAttribute;
+//      case TK_StringLiteral:
+//          return stringAttribute;
+//		case TK_SINGLE_LINE_COMMENT:
+//			return commentAttribute;
         default:
-		    if ((($CLASS_NAME_PREFIX$ParseController) controller).isKeyword(token.getKind()))
+		    if (((SimpleLPGParseController) controller).isKeyword(token.getKind()))
 				return keywordAttribute;
         	return super.getColoring(controller, token);
         }
     }
 
-    public $COLORER_CLASS_NAME$() {
-        super();
-        // TODO:  Define text attributes for the various
-        // token types that will have their text colored
-        //
-        // NOTE:  Colors (i.e., instances of org.eclipse.swt.graphics.Color) are system resources
-        // and are limited in number.  THEREFORE, it is good practice to reuse existing system Colors
-        // or to allocate a fixed set of new Colors and reuse those.  If new Colors are instantiated
-        // beyond the bounds of your system capacity then your Eclipse invocation may cease to function
-        // properly or at all.
-        Display display = Display.getDefault();
-        commentAttribute = new TextAttribute(display.getSystemColor(SWT.COLOR_DARK_RED), null, SWT.ITALIC);
-        stringAttribute = new TextAttribute(display.getSystemColor(SWT.COLOR_DARK_BLUE), null, SWT.BOLD);
-        identifierAttribute = new TextAttribute(display.getSystemColor(SWT.COLOR_BLACK), null, SWT.NORMAL);
-        doubleAttribute = new TextAttribute(display.getSystemColor(SWT.COLOR_DARK_GREEN), null, SWT.BOLD);
-        numberAttribute = new TextAttribute(display.getSystemColor(SWT.COLOR_DARK_YELLOW), null, SWT.BOLD);
-        keywordAttribute = new TextAttribute(display.getSystemColor(SWT.COLOR_DARK_MAGENTA), null, SWT.BOLD);
-    }
-
-    
     public IRegion calculateDamageExtent(IRegion seed) {
         return seed;
     }
-    
 }
