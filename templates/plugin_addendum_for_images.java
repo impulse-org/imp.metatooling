aaa02c5cacdaa306c9a47d8fe5ebe4f670aa72e7
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-03-21 18:21:15 +0000

    Simplified initializeImageRegistry() a bit.

--- templates/plugin_addendum_for_images.java
+++ templates/plugin_addendum_for_images.java
@@ -5,24 +5,25 @@
 		new org.eclipse.core.runtime.Path("icons/"); //$NON-NLS-1$("icons/"); //$NON-NLS-1$
 
     protected void initializeImageRegistry(org.eclipse.jface.resource.ImageRegistry reg) {
+        org.osgi.framework.Bundle bundle= getBundle();
     	org.eclipse.core.runtime.IPath path= ICONS_PATH.append("$CLASS_NAME_PREFIX_LOWER$_default_image.gif");//$NON-NLS-1$
-    	org.eclipse.jface.resource.ImageDescriptor imageDescriptor= createImageDescriptor(getInstance().getBundle(), path);
+    	org.eclipse.jface.resource.ImageDescriptor imageDescriptor= createImageDescriptor(bundle, path);
         reg.put(I$CLASS_NAME_PREFIX$Resources.$CLASS_NAME_PREFIX_UPPER$_DEFAULT_IMAGE, imageDescriptor);
 
         path= ICONS_PATH.append("$CLASS_NAME_PREFIX_LOWER$_default_outline_item.gif");//$NON-NLS-1$
-        imageDescriptor= createImageDescriptor(getInstance().getBundle(), path);
+        imageDescriptor= createImageDescriptor(bundle, path);
         reg.put(I$CLASS_NAME_PREFIX$Resources.$CLASS_NAME_PREFIX_UPPER$_DEFAULT_OUTLINE_ITEM, imageDescriptor);
 
         path= ICONS_PATH.append("$CLASS_NAME_PREFIX_LOWER$_file.gif");//$NON-NLS-1$
-        imageDescriptor= createImageDescriptor(getInstance().getBundle(), path);
+        imageDescriptor= createImageDescriptor(bundle, path);
         reg.put(I$CLASS_NAME_PREFIX$Resources.$CLASS_NAME_PREFIX_UPPER$_FILE, imageDescriptor);
 
         path= ICONS_PATH.append("$CLASS_NAME_PREFIX_LOWER$_file_warning.gif");//$NON-NLS-1$
-        imageDescriptor= createImageDescriptor(getInstance().getBundle(), path);
+        imageDescriptor= createImageDescriptor(bundle, path);
         reg.put(I$CLASS_NAME_PREFIX$Resources.$CLASS_NAME_PREFIX_UPPER$_FILE_WARNING, imageDescriptor);
 
         path= ICONS_PATH.append("$CLASS_NAME_PREFIX_LOWER$_file_error.gif");//$NON-NLS-1$
-        imageDescriptor= createImageDescriptor(getInstance().getBundle(), path);
+        imageDescriptor= createImageDescriptor(bundle, path);
         reg.put(I$CLASS_NAME_PREFIX$Resources.$CLASS_NAME_PREFIX_UPPER$_FILE_ERROR, imageDescriptor);
     }
 
