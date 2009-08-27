7971e05de523966b3c6b0f5ec75ec1e2a8061350
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-08-27 21:42:11 +0000

    Fixed trivial bug in isValid() - if super returned something interesting, it was ignored.

--- src/org/eclipse/imp/ui/dialogs/validators/SelectionValidatorForIDEProjects.java
+++ src/org/eclipse/imp/ui/dialogs/validators/SelectionValidatorForIDEProjects.java
@@ -22,6 +22,6 @@ public class SelectionValidatorForIDEProjects extends SelectionValidatorForPlugi
             if (!ValidationUtils.isIDEProject((IProject) selection))
                 return ERROR_MSG_NOT_IDE_PROJECT;
         }
-        return null;
+        return result;
     }
 }
