8dea1ea2b570313d28cc4d22f4e4fddbb7ae5925
Robert M. Fuhrer <rmfuhrer@optonline.net>
2010-04-06 20:57:58 +0000

    Added type arguments to Map var decl, to remove compiler warnings.

--- src/org/eclipse/imp/wizards/NewFoldingUpdater.java
+++ src/org/eclipse/imp/wizards/NewFoldingUpdater.java
@@ -1,18 +1,13 @@
 /*******************************************************************************
-* Copyright (c) 2007 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
-
-*******************************************************************************/
-
-/**
- * 
- */
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.imp.wizards;
 
 import java.util.Arrays;
@@ -25,31 +20,28 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.imp.runtime.RuntimePlugin;
 
 public class NewFoldingUpdater extends CodeServiceWizard {
-	public void addPages() {
-	    addPages(new ExtensionPointWizardPage[] { new ExtensionPointWizardPage(this, RuntimePlugin.IMP_RUNTIME, "foldingUpdater"), });
-	}
+    public void addPages() {
+        addPages(new ExtensionPointWizardPage[] {
+            new ExtensionPointWizardPage(this, RuntimePlugin.IMP_RUNTIME, "foldingUpdater"),
+        });
+    }
 
-	protected List getPluginDependencies() {
-	    return Arrays.asList(new String[] { "org.eclipse.core.runtime", "org.eclipse.core.resources",
-		    "org.eclipse.imp.runtime" });
-	}
+    protected List getPluginDependencies() {
+        return Arrays.asList(new String[] {
+                "org.eclipse.core.runtime", "org.eclipse.core.resources",
+                "org.eclipse.imp.runtime"
+        });
+    }
 
-	public void generateCodeStubs(IProgressMonitor mon) throws CoreException {
-            Map subs= getStandardSubstitutions();
-
-            subs.put("$PARSER_PKG$", fParserPackage);
-
-            subs.remove("$FOLDER_CLASS_NAME$");
-            subs.put("$FOLDER_CLASS_NAME$", fFullClassName);
-            
-            subs.remove("$PACKAGE_NAME$");
-            subs.put("$PACKAGE_NAME$", fPackageName);
-          
-            String folderTemplateName = fTemplateName;	//"folder.java";
-            IFile folderSrc = createFileFromTemplate(fFullClassName + ".java", folderTemplateName, fPackageFolder, subs, fProject, mon);
-            
-            editFile(mon, folderSrc);
-	}
-
-
+    public void generateCodeStubs(IProgressMonitor mon) throws CoreException {
+        Map<String, String> subs= getStandardSubstitutions();
+        subs.put("$PARSER_PKG$", fParserPackage);
+        subs.remove("$FOLDER_CLASS_NAME$");
+        subs.put("$FOLDER_CLASS_NAME$", fFullClassName);
+        subs.remove("$PACKAGE_NAME$");
+        subs.put("$PACKAGE_NAME$", fPackageName);
+        String folderTemplateName= fTemplateName; // "folder.java";
+        IFile folderSrc= createFileFromTemplate(fFullClassName + ".java", folderTemplateName, fPackageFolder, subs, fProject, mon);
+        editFile(mon, folderSrc);
+    }
 }
